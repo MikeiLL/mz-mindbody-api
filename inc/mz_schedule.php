8decf0e966ccbcf158ed8af3a027bca43ea362b3
Rob Taylor <rt@tdmedia.com>
2014-04-21 15:28:58 -0700

    simplifies object creation and credential sharing

--- inc/mz_schedule.php
+++ inc/mz_schedule.php
@@ -1,14 +1,7 @@
 <?php
 function mZ_mindbody_show_schedule() {
-require_once MZ_MINDBODY_SCHEDULE_DIR .'/mindbody-api/MB_API.php';
-$mb = new my_MB_API();
-$options = get_option( 'mz_mindbody_options','Error: Mindbody Credentials Not Set' );
-$sourceCredentials = array(
-		"SourceName"=>$options['mz_source_name'], 
-		"Password"=>$options['mz_mindbody_password'], 
-		"SiteIDs"=>array($options['mz_mindbody_siteID'])
-	);
-$mb->setCreds($sourceCredentials);
+
+	require_once MZ_MINDBODY_SCHEDULE_DIR .'inc/mz_mbo_init.inc';
 
 	$mz_timeframe = mz_mbo_schedule_nav($_GET);
 	//Send the timeframe to the GetClasses class
@@ -26,7 +19,7 @@ $mb->setCreds($sourceCredentials);
 		if (($class['IsCanceled'] == 'TRUE') && ($class['HideCancel'] == 'TRUE'))
 		{
 			continue; //skip this and move to next iteration
-		}  
+		}
 		?><tr><?php
 			$sDate = date('m/d/Y', strtotime($class['StartDateTime']));
 			$sLoc = $class['Location']['ID'];
@@ -42,7 +35,7 @@ $mb->setCreds($sourceCredentials);
 			$staffName = $class['Staff']['Name'];
 			$sessionType = $class['ClassDescription']['SessionType']['Name'];
 			echo "<td>".date('g:i a', strtotime($startDateTime))." - ".date('g:i a', strtotime($endDateTime))."<a class='btn' href='{$linkURL}' target='_newbrowser'>Sign-Up</a></td><td>";?>
-<!-- Link trigger modal -->				
+<!-- Link trigger modal -->
 <a data-toggle="modal" data-target="#myModal" href="<?php echo MZ_MINDBODY_SCHEDULE_URL ?>inc/modal_descriptions.php?classDescription=<?php echo urlencode(substr($classDescription, 0, 1000)) ?>"> <?php echo $className ?></a>
 
 <!-- Small modal -->
@@ -56,8 +49,8 @@ $mb->setCreds($sourceCredentials);
 			<?php
 					}
 				}
-				?>		
-	</table>    
+				?>
+	</table>
 	<?php mz_mbo_schedule_nav($_GET); ?>
 	</div>
 
@@ -76,8 +69,8 @@ $mb->setCreds($sourceCredentials);
 
 function mz_mbo_schedule_nav($mz_get_variables)
 {
-	$mz_schedule_page = get_permalink();	
-	//sanitize input	
+	$mz_schedule_page = get_permalink();
+	//sanitize input
 	//set week number based on php date or passed parameter from $_GET
 	$mz_weeknumber = empty($mz_get_variables['mz_week']) ? date("W", strtotime(date('Y-m-d'))) : mz_validate_weeknum($mz_get_variables['mz_week']);
 	//Navigate through the weeks
@@ -90,14 +83,14 @@ function mz_mbo_schedule_nav($mz_get_variables)
 		{
 			$mz_num_weeks_back = add_query_arg(array('mz_week' => ($mz_weeknumber - 1)));
 			$mz_num_weeks_forward = add_query_arg(array('mz_week' => ($mz_weeknumber + 1)));
-			echo ' <a href='.$mz_num_weeks_back.'>'.$mz_nav_weeks_text_prev.'</a>'; 
+			echo ' <a href='.$mz_num_weeks_back.'>'.$mz_nav_weeks_text_prev.'</a>';
 			echo ' - <a href='.$mz_schedule_page.'>'.$mz_nav_weeks_text_current.'</a> - ';
 			echo '<a href='.$mz_num_weeks_forward.'>'.$mz_nav_weeks_text_following.'</a>';
 			$mz_start_end_date = getStartandEndDate($mz_weeknumber,$mz_current_year);
 		}
 	else
 		{   //BOF following year
-			$mz_next_year = isset($mz_get_variables['mz_next_yr']) ? mz_validate_year($mz_get_variables['mz_next_yr']) : "1";	
+			$mz_next_year = isset($mz_get_variables['mz_next_yr']) ? mz_validate_year($mz_get_variables['mz_next_yr']) : "1";
 			$mz_weeknumber = ($mz_weeknumber > 40) ? $mz_weeknumber - ($num_weeks_in_year - 1) : $mz_weeknumber;
 			$from_the_future_backwards = ($mz_weeknumber == 2) ? $num_weeks_in_year : ($mz_weeknumber - 1);
 			$mz_num_weeks_forward = add_query_arg(array('mz_week' => ($mz_weeknumber + 1), 'mz_next_yr' => ($mz_current_year + 1)));
@@ -106,16 +99,16 @@ function mz_mbo_schedule_nav($mz_get_variables)
 				$mz_num_weeks_back = add_query_arg(array('mz_week' => ($num_weeks_in_year - 1)));
 				echo ' <a href='.$mz_num_weeks_back.'>'.$mz_nav_weeks_text_prev.'</a>';
 			}
-			else 
+			else
 			{
 				$mz_num_weeks_back = add_query_arg(array('mz_week' => ($mz_weeknumber - 1), 'mz_next_yr' => ($mz_current_year + 1)));
 				echo ' <a href='.$mz_num_weeks_back.'>'.$mz_nav_weeks_text_prev.'</a>';
-			} 
+			}
 			echo ' - <a href='.$mz_schedule_page.'>'.$mz_nav_weeks_text_current.'</a> - ';
-			echo '<a href='.$mz_num_weeks_forward.'>'.$mz_nav_weeks_text_following.'</a> ';	
+			echo '<a href='.$mz_num_weeks_forward.'>'.$mz_nav_weeks_text_following.'</a> ';
 		$mz_start_end_date = getStartandEndDate($mz_weeknumber,($mz_current_year +1));
 		}//EOF Following Year
 		$mz_timeframe = array('StartDateTime'=>$mz_start_end_date[0], 'EndDateTime'=>$mz_start_end_date[1]);
 		return $mz_timeframe;
-}		
+}
 ?>
