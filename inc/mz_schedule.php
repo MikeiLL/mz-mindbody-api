8032224cc20bb07468fc5182ad259cf9bc871fff
Rob Taylor <rt@tdmedia.com>
2014-04-24 22:12:50 -0700

    Refactors schedule to return at once. Fixes modal window display (see comments). Adds optional parameter to display single day sched.

--- inc/mz_schedule.php
+++ inc/mz_schedule.php
@@ -1,76 +1,124 @@
 <?php
-function mZ_mindbody_show_schedule() {
-
+function mZ_mindbody_show_schedule( $atts )
+{
 	require_once MZ_MINDBODY_SCHEDULE_DIR .'inc/mz_mbo_init.inc';
 
-	$mz_timeframe = mz_mbo_schedule_nav($_GET);
+	// optionally pass in a type parameter. Defaults to week.
+	extract( shortcode_atts( array(
+		'type' => 'week'
+			), $atts ) );
+
+	if ($type=='day')
+	{
+		$mz_timeframe = array('StartDateTime'=>date('Y-m-d'), 'EndDateTime'=>date('Y-m-d'), 'SchedNav'=>'');
+	}
+	else
+	{
+		$mz_timeframe = mz_mbo_schedule_nav($_GET);
+	}
+
 	//Send the timeframe to the GetClasses class
 	$data = $mb->GetClasses($mz_timeframe);
-	echo "<br/>";
-	//$mb->debug();
-	if(!empty($data['GetClassesResult']['Classes']['Class'])) {
-	$mz_days = $mb->makeNumericArray($data['GetClassesResult']['Classes']['Class']);
-	$mz_days = sortClassesByDate($mz_days);
-	$mz_date_display = "D F d";
-	?><div id="mz_mbo_schedule" class="mz_mbo_schedule"><table class="table table-striped"><?php
-	foreach($mz_days as $classDate => $mz_classes) {
-		?><tr><th><?php
-		echo date($mz_date_display, strtotime($classDate));
-		?></th><th>Class Name</th><th>Instructor</th><th>Class Type</th></tr><?php
-		foreach($mz_classes as $class) {
-		if (($class['IsCanceled'] == 'TRUE') && ($class['HideCancel'] == 'TRUE'))
+	$return = '';
+
+	if(!empty($data['GetClassesResult']['Classes']['Class']))
+	{
+		//$return .= $mb->debug();
+
+		$mz_days = $mb->makeNumericArray($data['GetClassesResult']['Classes']['Class']);
+		$mz_days = sortClassesByDate($mz_days);
+		$mz_date_display = "D F d";
+
+		$return .= '<div id="mz_mbo_schedule" class="mz_mbo_schedule">';
+		$return .= $mz_timeframe['SchedNav'];
+		$return .= '<table class="table table-striped">';
+
+		foreach($mz_days as $classDate => $mz_classes)
 		{
-			continue; //skip this and move to next iteration
+			$return .= '<tr><th>';
+			$return .= date($mz_date_display, strtotime($classDate));
+			$return .= '</th><th>Class Name</th><th>Instructor</th><th>Class Type</th></tr>';
+
+			foreach($mz_classes as $class)
+			{
+				if (!(($class['IsCanceled'] == 'TRUE') && ($class['HideCancel'] == 'TRUE')))
+				{
+					$sDate = date('m/d/Y', strtotime($class['StartDateTime']));
+					$sLoc = $class['Location']['ID'];
+					$sTG = $class['ClassDescription']['Program']['ID'];
+					$studioid = $class['Location']['SiteID'];
+					$sclassid = $class['ClassScheduleID'];
+					$classDescription = $class['ClassDescription']['Description'];
+					$sType = -7;
+					$linkURL = "https://clients.mindbodyonline.com/ws.asp?sDate={$sDate}&sLoc={$sLoc}&sTG={$sTG}&sType={$sType}&sclassid={$sclassid}&studioid={$studioid}";
+					$className = $class['ClassDescription']['Name'];
+					$startDateTime = date('Y-m-d H:i:s', strtotime($class['StartDateTime']));
+					$endDateTime = date('Y-m-d H:i:s', strtotime($class['EndDateTime']));
+					$staffName = $class['Staff']['Name'];
+					$sessionType = $class['ClassDescription']['SessionType']['Name'];
+					$isAvailable = $class['IsAvailable'];
+
+					// start building table rows
+					$return .= '<tr><td>';
+					$return .= date('g:i a', strtotime($startDateTime)) . ' - ' . date('g:i a', strtotime($endDateTime));
+					// only show the schedule button if enabled in MBO
+					$return .= $isAvailable ? '<br><a class="btn" href="' . $linkURL . '" target="_newbrowser">Sign-Up</a>' : '';
+
+					$return .= '</td><td>';
+
+					// trigger link modal
+					$return .= '<a data-toggle="modal" data-target="#mySmallModal" href="' . MZ_MINDBODY_SCHEDULE_URL . 'inc/modal_descriptions.php?classDescription=' . urlencode(substr($classDescription, 0, 1000)) . '">' . $className . '</a>';
+
+					$return .= '</td><td>';
+					$return .= $staffName;
+					$return .= '</td><td>';
+					$return .= $sessionType;
+
+					//$return .= $classDescription;
+					$return .= '</td></tr>';
+
+				} // EOF if
+			}// EOF foreach class
+		}// EOF foreach day
+
+		$return .= '</table>';
+
+		// schedule navigation
+		$return .= $mz_timeframe['SchedNav'];
+
+		// modal-content needs to live here for dynamic loading to work
+		// this still doesn't work because content is only loaded on
+		// the first click.  Not sure how to force content reload each click
+		$return .= '<div id="mySmallModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+			<div class="modal-dialog modal-sm">
+        <div class="modal-content">
+
+				</div>
+		</div>';
+
+		$return .= '</div>';
+	}
+	else
+	{
+
+		if(!empty($data['GetClassesResult']['Message']))
+		{
+			$return = $data['GetClassesResult']['Message'];
 		}
-		?><tr><?php
-			$sDate = date('m/d/Y', strtotime($class['StartDateTime']));
-			$sLoc = $class['Location']['ID'];
-			$sTG = $class['ClassDescription']['Program']['ID'];
-			$studioid = $class['Location']['SiteID'];
-			$sclassid = $class['ClassScheduleID'];
-			$classDescription = $class['ClassDescription']['Description'];
-			$sType = -7;
-			$linkURL = "https://clients.mindbodyonline.com/ws.asp?sDate={$sDate}&sLoc={$sLoc}&sTG={$sTG}&sType={$sType}&sclassid={$sclassid}&studioid={$studioid}";
-			$className = $class['ClassDescription']['Name'];
-			$startDateTime = date('Y-m-d H:i:s', strtotime($class['StartDateTime']));
-			$endDateTime = date('Y-m-d H:i:s', strtotime($class['EndDateTime']));
-			$staffName = $class['Staff']['Name'];
-			$sessionType = $class['ClassDescription']['SessionType']['Name'];
-			echo "<td>".date('g:i a', strtotime($startDateTime))." - ".date('g:i a', strtotime($endDateTime))."<a class='btn' href='{$linkURL}' target='_newbrowser'>Sign-Up</a></td><td>";?>
-<!-- Link trigger modal -->
-<a data-toggle="modal" data-target="#myModal" href="<?php echo MZ_MINDBODY_SCHEDULE_URL ?>inc/modal_descriptions.php?classDescription=<?php echo urlencode(substr($classDescription, 0, 1000)) ?>"> <?php echo $className ?></a>
-
-<!-- Small modal -->
-
-<div id="myModal" class='modal fade bs-example-modal-sm' tabindex='-1' role='dialog' aria-labelledby='mySmallModalLabel' aria-hidden='true'>
-
-</div>
-
-</td><td><?php echo" {$staffName} </td><td>".$sessionType."</td>";
-					?></tr>
-			<?php
-					}
-				}
-				?>
-	</table>
-	<?php mz_mbo_schedule_nav($_GET); ?>
-	</div>
-
-
-	<?php
-} else {
-		if(!empty($data['GetClassesResult']['Message'])) {
-			echo $data['GetClassesResult']['Message'];
-		} else {
-			echo "__(Error getting classes. Try re-loading the page.)<br />";
-			echo '<pre>'.print_r($data,1).'</pre>';
+		else
+		{
+			$return = "__(Error getting classes. Try re-loading the page.)<br />";
+			$return .= '<pre>'.print_r($data,1).'</pre>';
 		}
 	}//EOF If Result / Else
+
+	return $return;
 
 }//EOF mZ_show_schedule
 
 function mz_mbo_schedule_nav($mz_get_variables)
 {
+	$sched_nav = '';
 	$mz_schedule_page = get_permalink();
 	//sanitize input
 	//set week number based on php date or passed parameter from $_GET
@@ -82,35 +130,37 @@ function mz_mbo_schedule_nav($mz_get_variables)
 	$mz_current_year = date("Y");
 	$num_weeks_in_year =  weeknumber($mz_current_year, 12, 31);
 	if (($mz_weeknumber < $num_weeks_in_year) && empty($mz_get_variables['mz_next_yr']))
-		{
-			$mz_num_weeks_back = add_query_arg(array('mz_week' => ($mz_weeknumber - 1)));
-			$mz_num_weeks_forward = add_query_arg(array('mz_week' => ($mz_weeknumber + 1)));
-			echo ' <a href='.$mz_num_weeks_back.'>'.$mz_nav_weeks_text_prev.'</a>';
-			echo ' - <a href='.$mz_schedule_page.'>'.$mz_nav_weeks_text_current.'</a> - ';
-			echo '<a href='.$mz_num_weeks_forward.'>'.$mz_nav_weeks_text_following.'</a>';
-			$mz_start_end_date = getStartandEndDate($mz_weeknumber,$mz_current_year);
-		}
+	{
+		$mz_num_weeks_back = add_query_arg(array('mz_week' => ($mz_weeknumber - 1)));
+		$mz_num_weeks_forward = add_query_arg(array('mz_week' => ($mz_weeknumber + 1)));
+		$sched_nav .= ' <a href='.$mz_num_weeks_back.'>'.$mz_nav_weeks_text_prev.'</a>';
+		$sched_nav .= ' - <a href='.$mz_schedule_page.'>'.$mz_nav_weeks_text_current.'</a> - ';
+		$sched_nav .= '<a href='.$mz_num_weeks_forward.'>'.$mz_nav_weeks_text_following.'</a>';
+		$mz_start_end_date = getStartandEndDate($mz_weeknumber,$mz_current_year);
+	}
 	else
-		{   //BOF following year
-			$mz_next_year = isset($mz_get_variables['mz_next_yr']) ? mz_validate_year($mz_get_variables['mz_next_yr']) : "1";
-			$mz_weeknumber = ($mz_weeknumber > 40) ? $mz_weeknumber - ($num_weeks_in_year - 1) : $mz_weeknumber;
-			$from_the_future_backwards = ($mz_weeknumber == 2) ? $num_weeks_in_year : ($mz_weeknumber - 1);
-			$mz_num_weeks_forward = add_query_arg(array('mz_week' => ($mz_weeknumber + 1), 'mz_next_yr' => ($mz_current_year + 1)));
-			if ($mz_weeknumber == 1)
-			{//if we are in first week of year
-				$mz_num_weeks_back = add_query_arg(array('mz_week' => ($num_weeks_in_year - 1)));
-				echo ' <a href='.$mz_num_weeks_back.'>'.$mz_nav_weeks_text_prev.'</a>';
-			}
-			else
-			{
-				$mz_num_weeks_back = add_query_arg(array('mz_week' => ($mz_weeknumber - 1), 'mz_next_yr' => ($mz_current_year + 1)));
-				echo ' <a href='.$mz_num_weeks_back.'>'.$mz_nav_weeks_text_prev.'</a>';
-			}
-			echo ' - <a href='.$mz_schedule_page.'>'.$mz_nav_weeks_text_current.'</a> - ';
-			echo '<a href='.$mz_num_weeks_forward.'>'.$mz_nav_weeks_text_following.'</a> ';
+	{   //BOF following year
+		$mz_next_year = isset($mz_get_variables['mz_next_yr']) ? mz_validate_year($mz_get_variables['mz_next_yr']) : "1";
+		$mz_weeknumber = ($mz_weeknumber > 40) ? $mz_weeknumber - ($num_weeks_in_year - 1) : $mz_weeknumber;
+		$from_the_future_backwards = ($mz_weeknumber == 2) ? $num_weeks_in_year : ($mz_weeknumber - 1);
+		$mz_num_weeks_forward = add_query_arg(array('mz_week' => ($mz_weeknumber + 1), 'mz_next_yr' => ($mz_current_year + 1)));
+		if ($mz_weeknumber == 1)
+		{//if we are in first week of year
+			$mz_num_weeks_back = add_query_arg(array('mz_week' => ($num_weeks_in_year - 1)));
+			$sched_nav .= ' <a href='.$mz_num_weeks_back.'>'.$mz_nav_weeks_text_prev.'</a>';
+		}
+		else
+		{
+			$mz_num_weeks_back = add_query_arg(array('mz_week' => ($mz_weeknumber - 1), 'mz_next_yr' => ($mz_current_year + 1)));
+			$sched_nav .= ' <a href='.$mz_num_weeks_back.'>'.$mz_nav_weeks_text_prev.'</a>';
+		}
+		$sched_nav .= ' - <a href='.$mz_schedule_page.'>'.$mz_nav_weeks_text_current.'</a> - ';
+		$sched_nav .= '<a href='.$mz_num_weeks_forward.'>'.$mz_nav_weeks_text_following.'</a> ';
 		$mz_start_end_date = getStartandEndDate($mz_weeknumber,($mz_current_year +1));
-		}//EOF Following Year
-		$mz_timeframe = array('StartDateTime'=>$mz_start_end_date[0], 'EndDateTime'=>$mz_start_end_date[1]);
-		return $mz_timeframe;
+	}//EOF Following Year
+
+	$mz_timeframe = array('StartDateTime'=>$mz_start_end_date[0], 'EndDateTime'=>$mz_start_end_date[1], 'SchedNav'=>$sched_nav);
+
+	return $mz_timeframe;
 }
 ?>
