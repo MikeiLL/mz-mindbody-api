b50c76e7998dd617fa6ad8127530b70601d6f86d
Rob Taylor <rt@tdmedia.com>
2014-09-15 07:11:52 -0700

    fixes modal javascript

--- js/mz_mbo_modal.js
+++ js/mz_mbo_modal.js
@@ -1,21 +1,8 @@
 (function($){
-$(document).ready(function($) {
-		$("a[data-target=#myModal]").click(function(ev) {
-			ev.preventDefault();
-			var target = $(this).attr("href");
-			// load the url and show modal on success
-			$("#myModal").load(target, function() { 
-				 $("#myModal").modal({show:true}); 
-			});
-		});
-	});	
+
+    // kill modal contents on hide
+    $('body').on('hidden.bs.modal', '#mzModal', function () {
+      $(this).removeData('bs.modal');
+    });
+
 })(jQuery);
-/*
-http://stackoverflow.com/questions/20590227/twitter-bootstrap-how-to-clear-out-a-modal
-$(document).on 'click', '.close', ->
-    $(this).parent().fadeOut -> $(this).html('')
-http://stackoverflow.com/questions/12286332/twitter-bootstrap-remote-modal-shows-same-content-everytime
-    $('body').on('hidden.bs.modal', '.modal', function () {
-  $(this).removeData('bs.modal');
-});
-*/
