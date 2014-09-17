412b94fda35dceb07c20a71b46325788a08da37f
Mike iLL <mike@mzoo.org>
2014-09-16 20:03:45 -0500

    Moved // kill modal contents on hide inside of document.ready

--- js/mz_mbo_modal.js
+++ js/mz_mbo_modal.js
@@ -7,17 +7,12 @@ $(document).ready(function($) {
 			$("#mzModal").load(target, function() { 
 				 $("#mzModal").modal({show:true});  
 			});
-				
+			// kill modal contents on hide
+            $('body').on('hidden.bs.modal', '#mzModal', function () {
+             $(this).removeData('bs.modal');
+           });	
 		});
 		
 	});	
 
 })(jQuery);
- 
-    // kill modal contents on hide
-    $('body').on('hidden.bs.modal', '#mzModal', function () {
-      $(this).removeData('bs.modal');
-    });
-
-})(jQuery);
-
