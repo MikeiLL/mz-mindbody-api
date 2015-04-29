5735e0fac3460df4b0ef8fc8399d09e2c3228472
Mike iLL Kilmer <mike@mzoo.org>
2015-04-29 12:28:34 -0500

    Working demo with preset parameters.

--- inc/mz_mbo_add_to_classes.php
+++ inc/mz_mbo_add_to_classes.php
@@ -1,22 +1,36 @@
 <?php
-?>
-<!--<script type='text/javascript'>
-/* <![CDATA[ */
-var mz_mbo_params = {"ajaxurl":"http:\/\/localhost:8888\/wp-admin\/admin-ajax.php"};
-/* ]]> */
-</script>-->
-<?php
 function mz_mindbody_add_to_classes() {
 	require_once MZ_MINDBODY_SCHEDULE_DIR .'inc/mz_mbo_init.inc';
-	$clientID = 2260;
-    	$classID = 100015619;
-
-	$signupData = $mb->AddClientsToClasses(array($classID), array($clientID));
-
-	$mb->getXMLRequest();
-	$mb->getXMLResponse();
+	$clientIDs = array(100004590);
+    $classIDs = array(14891);
+    
+	$additions['ClassIDs'] = $classIDs;
+	$additions['ClientIDs'] = $clientIDs;
+	$signupData = $mb->AddClientsToClasses($additions);
+	echo "<pre>";
+	print_r($signupData);
+	echo "</pre>";
+	//$mb->getXMLRequest();
+	//$mb->getXMLResponse();
 	$mb->debug();
-	
+	if ( $signupData['AddClientsToClassesResult']['ErrorCode'] != 200){
+		echo "<h3>I'm sorry. We were unable to add you to the class.</h3>";
+		foreach ($signupData['AddClientsToClassesResult']['Classes']['Class']['Clients']['Client']['Messages'] as $message){
+					echo "<h4>". $message ."</h4>";
+			}
+			echo "<pre>";
+			echo getcwd() . "\n";
+			
+			echo "</pre>";
+		}else{
+			$classDetails = $signupData['AddClientsToClassesResult']['Classes']['Class'];
+			echo $classDetails['ClassDescription']['Name'];
+			echo "<hr>";
+			echo $classDetails['Staff']['Name'];
+			echo "<hr>";
+			echo $classDetails['Location']['Name'];
+			echo "<hr>";
+			echo $classDetails['Location']['Address'];
+		}
 	}
-
 ?>
