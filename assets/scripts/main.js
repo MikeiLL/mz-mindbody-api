04397817dd8ac38599144d0c45e80585f4fc4c13
Mike iLL Kilmer <mike@mzoo.org>
2015-06-02 15:39:27 -0500

    Step two working in Node, Gulp and Bower.

---
+++ assets/scripts/main.js
@@ -0,0 +1,33 @@
+(function($){
+$(document).ready(function($) {
+		$("a[data-target=#mzModal]").click(function(ev) {
+			ev.preventDefault();
+			var target = $(this).attr("href");
+			// load the url and show modal on success
+			$("#mzModal").load(target, function() { 
+				 $("#mzModal").modal({show:true});  
+			});
+			// kill modal contents on hide
+			    $('body').on('hidden.bs.modal', '#mzModal', function () {
+			     $(this).removeData('bs.modal');
+			   });	
+		});
+	});
+	
+$(document).ready(function($) {
+		$("a[data-target=#mzModal]").click(function(ev) {
+			ev.preventDefault();
+			var target = $(this).attr("href");
+			// load the url and show modal on success
+			$("#mzModal").load(target, function() { 
+				 $("#mzModal").modal({show:true});  
+			});
+			// kill modal contents on hide
+			    $('body').on('hidden.bs.modal', '#mzModal', function () {
+			     $(this).removeData('bs.modal');
+			   });	
+		});
+	});
+	
+})(jQuery);
+
