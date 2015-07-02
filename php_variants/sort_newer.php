e5c7397ee9661aedfad85466bcfe5dc4542262d5
Mike iLL Kilmer <mike@mzoo.org>
2015-07-02 12:42:01 -0500

    Only add items to array if conditions are met.

--- php_variants/sort_newer.php
+++ php_variants/sort_newer.php
@@ -30,11 +30,11 @@ function sortClassesByDate($mz_classes = array(), $time_format = "g:i a") {
 	return $mz_classesByDate;
 }
 
-function sortClassesByTimeThenDay($mz_classes = array(), $time_format = "g:i a") {
+function sortClassesByTimeThenDay($mz_classes = array(), $time_format = "g:i a", $location = 1) {
 	$mz_classesByTime = array();
-
-	foreach($mz_classes as &$class)
+	foreach($mz_classes as $class)
 	{
+		
 		/* Create a new array with a key for each time
 		and corresponsing value an array of class details 
 		for classes at that time. */ 
@@ -42,14 +42,28 @@ function sortClassesByTimeThenDay($mz_classes = array(), $time_format = "g:i a")
 		// $class['day_num'] = '';
 		$class['day_num'] = date_i18n("N", strtotime($class['StartDateTime'])); // Weekday num 1-7
 		if(!empty($mz_classesByTime[$classTime])) {
+			if (
+				($class['Location']['ID'] != $location) || 
+				(($class['IsCanceled'] == 1) && ($class['HideCancel'] == 1)) ||
+				($class['ClassDescription']['Program']['ScheduleType'] == 'Enrollment')
+				) {
+					continue;
+				}
 			$mz_classesByTime[$classTime]['classes'] = array_merge($mz_classesByTime[$classTime]['classes'], array($class));
 		} else {
-
+			if (
+				($class['Location']['ID'] != $location) || 
+				(($class['IsCanceled'] == 1) && ($class['HideCancel'] == 1)) ||
+				($class['ClassDescription']['Program']['ScheduleType'] == 'Enrollment')
+				) {
+					continue;
+				}
 			$display_time = (date_i18n($time_format, strtotime($class['StartDateTime']))); 
 			$mz_classesByTime[$classTime] = array('display_time' => $display_time, 
 													'classes' => array($class));
 		}
 	}
+
 	/* Timeslot keys in new array are not time-sequenced so do so*/
 	ksort($mz_classesByTime);
 	foreach($mz_classesByTime as $scheduleTime => &$mz_classes)
