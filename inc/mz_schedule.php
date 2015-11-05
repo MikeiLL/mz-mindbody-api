d60ceb11bfbf61e6e2b151623f0dc7978fcbcf86
Mike iLL Kilmer <mike@mzoo.org>
2015-11-05 10:49:16 -0600

    Wrap filtertable in noconflict code.

--- inc/mz_schedule.php
+++ inc/mz_schedule.php
@@ -492,26 +492,33 @@ class MZ_Mindbody_Schedule_Display {
 			));
 
 		?>
+		<?php if ($filter == 1): ?>
+		<!-- Start mZ_mindbody-api filterTable configuration -->
 		<script type="text/javascript">
+			(function($) {
 			$(document).ready(function() {
-			var stripeTable = function(table) { //stripe the table (jQuery selector)
-					table.find('tr').removeClass('striped').filter(':visible:even').addClass('striped');
-				};
+				var stripeTable = function(table) { //stripe the table (jQuery selector)
+						table.find('tr').removeClass('striped').filter(':visible:even').addClass('striped');
+					};
 
-				$('table.mz-schedule-filter').filterTable({
-					callback: function(term, table) { stripeTable(table); }, //call the striping after every change to the filter term
-					placeholder: mz_mindbody_api_i18n.filter_default,
-					highlightClass: 'alt',
-					inputType: 'search',
-					label: mz_mindbody_api_i18n.label,
-					selector: mz_mindbody_api_i18n.selector,
-					quickListClass: 'mz_quick_filter',
-					quickList: [mz_mindbody_api_i18n.quick_1, mz_mindbody_api_i18n.quick_2, mz_mindbody_api_i18n.quick_3],
-					locations: mz_mindbody_api_i18n.Locations_dict
+					$('table.mz-schedule-filter').filterTable({
+						callback: function(term, table) { stripeTable(table); }, //call the striping after every change to the filter term
+						placeholder: mz_mindbody_api_i18n.filter_default,
+						highlightClass: 'alt',
+						inputType: 'search',
+						label: mz_mindbody_api_i18n.label,
+						selector: mz_mindbody_api_i18n.selector,
+						quickListClass: 'mz_quick_filter',
+						quickList: [mz_mindbody_api_i18n.quick_1, mz_mindbody_api_i18n.quick_2, mz_mindbody_api_i18n.quick_3],
+						locations: mz_mindbody_api_i18n.Locations_dict
+					});
+					stripeTable($('table.mz-schedule-filter')); //stripe the table for the first time
 				});
-				stripeTable($('table.mz-schedule-filter')); //stripe the table for the first time
-			});
+			})(jQuery);
 		</script>
+		<!-- End mZ_mindbody-api filterTable configuration -->
+		<?php endif; ?>
+		
 		<?php
 	}
 	
