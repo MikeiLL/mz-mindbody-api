f2eb452ded0c87d6624122660d2da7f69ee98ebb
Mike iLL Kilmer <mike@mzoo.org>
2015-12-07 20:27:54 -0600

    Display registrants in colorbox modal.

--- inc/modal_descriptions.php
+++ inc/modal_descriptions.php
@@ -3,19 +3,5 @@ if (isset($_GET["className"]))
   {
     $modal_name = $_GET["className"];
 ?>
-<div class="modal-dialog modal-wide mz-modal">
-    <div class="modal-header">
-      <button type="button" class="close" id="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-      <h4 class="modal-title" id="mzSmallModalLabel"><?php echo stripslashes($modal_name)?></h4>
-    </div>
-     <div class="modal-body">
-      <span>
-     	</span>
-      <?php echo stripslashes($modal_description)?>
-      <div id="ClassRegistrants">
-      </div>
-    </div>
-    <div class="modal-footer">
-    </div>
-</div>
+
 <?php } ?>
