55defe9ca731ead409edaf89311c39fc28752f90
Mike iLL Kilmer <mike@mzoo.org>
2016-02-24 15:41:44 -0600

    Keep schedule objects here.

---
+++ lib/schedule_objects.php
@@ -0,0 +1,113 @@
+<?php
+
+class One_Week {
+
+	public $schedule=array();
+	
+	public function __construct(){
+		
+	}
+	
+}
+
+class Single_day {
+
+	public $day_number;
+	public $classes = array();
+	
+	public function __construct($day){
+		$this->day_number = $day;
+	}
+	
+}
+
+class Time_slot {
+
+	public $day_num;
+	public $classes = array();
+	public $time_slot;
+	public $display_time;
+	
+	public function __construct($time, $display_time){
+		$this->time_slot = $time;
+		$this->display_time = $display_time;
+	}
+	
+}
+
+class Classes_by_time {
+
+	public $day_num;
+	public $classes = array();
+	public $time_slot;
+	public $display_time;
+	
+	public function __construct($time, $display_time){
+		$this->time_slot = $time;
+		$this->display_time = $display_time;
+	}
+	
+}
+
+class Single_event {
+
+	public $sDate;
+	public $sLoc;
+	public $sTG;
+	public $studioid;
+	public $sclassid;
+	public $sclassidID;
+	public $sessionTypeName;
+	public $classDescription;
+	public $classImage;
+	public $classImageArray;
+	public $displayCancelled;
+	public $className;
+	public $startDateTime;
+	public $endDateTime;
+	public $staffName;
+	public $isAvailable;
+	public $locationName;
+	public $staffImage;
+	public $day_num; //for use in grid schedule display
+	
+	public function __construct($class, $day_num = ''){
+		$this->sDate = date_i18n('m/d/Y', strtotime($class['StartDateTime']));
+		$this->sLoc = $class['Location']['ID'];
+		$this->sTG = $class['ClassDescription']['Program']['ID'];
+		$this->studioid = $class['Location']['SiteID'];
+		$this->sclassid = $class['ClassScheduleID'];
+		$this->sclassidID = $class['ID'];
+		$this->sessionTypeName = $class['ClassDescription']['SessionType']['Name'];
+								//mz_pr($sclassidID);
+		$this->classDescription = $class['ClassDescription']['Description'];
+						
+								//Let's find an image if there is one and assign it to $classImage
+						
+								if (!isset($class['ClassDescription']['ImageURL'])) {
+			$this->classImage = '';
+									if (isset($class['ClassDescription']['AdditionalImageURLs']) && !empty($classImageArray)) {
+				$this->classImage = pop($classImageArray);
+										}
+								} else {
+			$this->classImage = $class['ClassDescription']['ImageURL'];
+								}
+
+		$this->sType = -7;
+		$this->displayCancelled = ($class['IsCanceled'] == 1) ? '<div class="mz_cancelled_class">' .
+												__('Cancelled', 'mz-mindbody-api') . '</div>' : '';
+		$this->className = $class['ClassDescription']['Name'];
+								//mz_pr($className);
+		$this->startDateTime = date_i18n('Y-m-d H:i:s', strtotime($class['StartDateTime']));
+								//mz_pr($startDateTime);
+								//echo "<hr/>";
+		$this->endDateTime = date_i18n('Y-m-d H:i:s', strtotime($class['EndDateTime']));
+		$this->staffName = $class['Staff']['Name'];
+		$this->isAvailable = $class['IsAvailable'];
+		$this->locationName = $class['Location']['Name'];
+		$this->staffImage = isset($class['Staff']['ImageURL']) ? $class['Staff']['ImageURL'] : '';
+		$this->day_num = $day_num;
+	}
+	
+}
+?>
