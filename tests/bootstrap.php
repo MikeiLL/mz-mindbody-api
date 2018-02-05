cdb4d58ef15d3be36b553620fcf716029208a4c2
Mike iLL Kilmer <mike@mzoo.org>
2018-02-05 17:44:04 -0600

    Initial with chrits zarate latest.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,22 +6,23 @@
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	throw new Exception( "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" );
 }
 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
-//Pull in our Autoloader
-require __DIR__ . "/Autoload.php";
-spl_autoload_register( [ new MBOLoader\Autoload( 'MBOTests', __DIR__ . '' ), 'load' ] );
-
 /**
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/mZ-mindbody-api.php';
+	require dirname( dirname( __FILE__ ) ) . '/mz-mindbody-api.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
