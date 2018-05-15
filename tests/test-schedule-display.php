d9c81e5873765d44f7142ba9bfdd69a59e70efbb
Mike iLL Kilmer <mike@mzoo.org>
2018-05-14 19:11:35 -0500

    Manually initialize $basic_options static var so test passes.

--- tests/test-schedule-display.php
+++ tests/test-schedule-display.php
@@ -17,24 +17,23 @@ class ScheduleDisplayTest extends WP_UnitTestCase {
 	 */
 	function test_get_classes_result() {			
 		parent::setUp();
-		$options = array(
+		$basic_options = array(
 			'mz_source_name' => MBOTests\Test_Options::$_MYSOURCENAME,
 			'mz_mindbody_password' => MBOTests\Test_Options::$_MYPASSWORD,
-			'mz_mindbody_siteID' => '-99',
-			'mz_mindbody_eventID' => ''
+			'mz_mindbody_siteID' => '-99'
 		);
-		add_option( 'mz_mindbody_options', $options, '', 'yes' );
-		var_dump(MZ_Mindbody\Inc\Core\Init::$basic_options);
+        add_option( 'mz_mbo_basic', $basic_options, '', 'yes' );
+        // Manually initialize static variable.
+		MZ_Mindbody\Inc\Core\Init::$basic_options = get_option('mz_mbo_basic');
 		$schedule_object = new MZ_Mindbody\Inc\Schedule\Retrieve_Schedule;
 		$time_frame = $schedule_object->time_frame();
         $this->assertTrue(count($time_frame) === 2);
 	  	$response = $schedule_object->get_mbo_results();
 		$this->assertTrue(is_array($response));
-		//var_dump($response);
         $this->assertTrue($response['GetClassesResult']['ErrorCode'] == 200);
-		//$this->assertTrue(is_array($mbo['GetClassesResult']['Classes']));
-		//$this->assertTrue(is_array($mbo['GetClassesResult']['Classes']['Class']));
-		//$this->assertTrue($mbo['GetClassesResult']['Classes']['Class'][0]['Location']['SiteID'] == '-99');
+		$this->assertTrue(is_array($response['GetClassesResult']['Classes']));
+		$this->assertTrue(is_array($response['GetClassesResult']['Classes']['Class']));
+		$this->assertTrue($response['GetClassesResult']['Classes']['Class'][0]['Location']['SiteID'] == '-99');
 		parent::tearDown();
 	}
 	
