ffb7d1f0ee6a5cc8926ccc5bf6f8474b9bd34e66
Mike iLL Kilmer <mike@mzoo.org>
2018-05-17 14:28:19 -0500

    Add navigation with jQuery click function.

--- assets/scripts/schedule-display.js
+++ assets/scripts/schedule-display.js
@@ -24,5 +24,31 @@
         container.toggleClass('loader');
         container.html('Sorry but there was an error retrieving events.');
     }); // End Ajax
-  });
+
+    $('#mzScheduleNavHolder .following').on('click',function() {
+        container.toggleClass('loader');
+        container.html("");
+        $.ajax({
+        type : "post",
+        dataType : "json",
+        url : mz_mindbody_schedule.ajaxurl,
+        data : {action: 'mz_display_schedule', nonce: nonce, atts: atts},
+        success: function(json) {
+            if(json.type == "success") {
+                container.toggleClass('loader');
+                container.html(json.message);
+            } else {
+                container.toggleClass('loader');
+                container.html(json.message);
+            }
+        }
+    })
+        .fail( function( json ) {
+            console.log('fail');
+            console.log(json);
+            container.toggleClass('loader');
+            container.html('Sorry but there was an error retrieving events.');
+        }); // End Ajax
+    }); // End click following
+  }); // End document ready
 })( jQuery );
--- inc/frontend/views/schedule_container.php
+++ inc/frontend/views/schedule_container.php
@@ -1,1 +1,5 @@
+<div id="mzScheduleNavHolder">
+    <a class="previous">Previous Week</a> - <a class="following">Following Week</a>
+</div>
+
 <div id="mzScheduleDisplay" class="loader"></div>
