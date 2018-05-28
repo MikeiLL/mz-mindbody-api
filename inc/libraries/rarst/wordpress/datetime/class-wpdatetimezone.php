daa3c51fa0c9272bcc2dd0aa1d45ba9e728a758a
Mike iLL Kilmer <mike@mzoo.org>
2018-05-27 19:43:04 -0500

    Add new library for Wordpress DateTime objects.

---
+++ inc/libraries/rarst/wordpress/datetime/class-wpdatetimezone.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MZ_Mindbody\Inc\Libraries\Rarst\WordPress\DateTime;
+
+/**
+ * Extension of DateTimeZone for WordPress.
+ */
+class WpDateTimeZone extends \DateTimeZone {
+
+	/**
+	 * Determine time zone from WordPress options and return as object.
+	 *
+	 * @return static
+	 */
+	public static function getWpTimezone() {
+
+		$timezone_string = get_option( 'timezone_string' );
+
+		if ( ! empty( $timezone_string ) ) {
+			return new static( $timezone_string );
+		}
+
+		$offset  = get_option( 'gmt_offset' );
+		$hours   = (int) $offset;
+		$minutes = abs( ( $offset - (int) $offset ) * 60 );
+		$offset  = sprintf( '%+03d:%02d', $hours, $minutes );
+
+		return new static( $offset );
+	}
+}
