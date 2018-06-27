1a745985de18cbe0cefc4fb8ddaea938b7bd553e
Mike iLL Kilmer <mike@mzoo.org>
2018-06-27 14:40:26 -0500

    Use include for footer.

---
+++ inc/frontend/views/modal_footer.php
@@ -0,0 +1,5 @@
+<div class="modal__footer">
+        <a style="float:right;margin:0 1em" class="btn btn-primary" id="MBOLogout"><?php _e('Logout', 'mz-mindbody-api'); ?></a>
+        <a style="float:right;margin:0 1em" class="btn btn-primary" id="MBOSite"><?php _e('Manage on Mindbody Site', 'mz-mindbody-api'); ?></a>
+        <a style="float:right;margin:0 1em" class="btn btn-primary" data-nonce="<?php echo $data->nonce; ?>" id="MBOSchedule"><?php _e('My Classes', 'mz-mindbody-api'); ?></a>
+    </div>
