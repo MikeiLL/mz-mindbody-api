a5d6cabacbcca2bfb301c8411bb41bdbf5dc5dbb
Mike iLL Kilmer <mike@mzoo.org>
2018-07-03 17:58:01 -0500

    Include a meaningless test so warning goes away.

--- tests/class-mzmbo-wpunittestcase.php
+++ tests/class-mzmbo-wpunittestcase.php
@@ -8,9 +8,13 @@
 /**
  * Add a logging method to WP UnitTestCase Class.
  */
-class MZMBO_UnitTestCase extends WP_UnitTestCase
-{
-    public function el($message){
-        file_put_contents('./log_'.date("j.n.Y").'.log', $message, FILE_APPEND);
+    class MZMBO_UnitTestCase extends WP_UnitTestCase
+    {
+        public function el($message){
+            file_put_contents('./log_'.date("j.n.Y").'.log', $message, FILE_APPEND);
+        }
+
+        public function test_nothing() {
+            $this->assertEquals( true, true );
+        }
     }
-}
