37c9ea14d25002d29986d8d868ee9e555b551466
Mike iLL Kilmer <mike@mzoo.org>
2018-07-13 17:11:32 -0500

    Add spaces between label and field in signup form.

--- inc/frontend/views/create_mbo_account.php
+++ inc/frontend/views/create_mbo_account.php
@@ -10,10 +10,10 @@
             <p class="website_url" style="display:none"><?php echo $antispam; ?><input type="text" name="website_url" /></p>
             <input type="hidden" name="nonce" value="<?php echo $data->nonce; ?>"/>
             <input type="hidden" name="classID" value="<?php echo $data->classID; ?>" />
-            <label for="Username"> <?php echo $data->username; ?></label><input type="text" name="data[Client][Username]" id="Username" placeholder="<?php echo $data->username; ?>" required /><br />
-            <label for="Password"> <?php echo $data->password; ?></label><input type="password" name="data[Client][Password]" id="Password" placeholder="<?php echo $data->password; ?>" required /><br />
-            <label for="FirstName"> <?php echo $data->firstname; ?></label><input type="text" name="data[Client][FirstName]" id="FirstName" placeholder="<?php echo $data->firstname; ?>" required /><br />
-            <label for="LastName"> <?php echo $data->lastname; ?></label><input type="text" name="data[Client][LastName]" id="LastName" placeholder="<?php echo $data->lastname; ?>" required /><br />
+            <label for="Username"> <?php echo $data->username; ?></label> <input type="text" name="data[Client][Username]" id="Username" placeholder="<?php echo $data->username; ?>" required /><br />
+            <label for="Password"> <?php echo $data->password; ?></label> <input type="password" name="data[Client][Password]" id="Password" placeholder="<?php echo $data->password; ?>" required /><br />
+            <label for="FirstName"> <?php echo $data->firstname; ?></label> <input type="text" name="data[Client][FirstName]" id="FirstName" placeholder="<?php echo $data->firstname; ?>" required /><br />
+            <label for="LastName"> <?php echo $data->lastname; ?></label> <input type="text" name="data[Client][LastName]" id="LastName" placeholder="<?php echo $data->lastname; ?>" required /><br />
             <?php echo $data->requiredFieldsInputs; ?><br />
             <button type="submit" class="btn btn-primary"><?php echo $data->sign_up; ?></button>
 
