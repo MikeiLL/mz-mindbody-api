d5e66ebcbd132987b7f303de74334eb40e2e6e35
Mike iLL Kilmer <mike@mzoo.org>
2018-07-17 18:08:10 -0500

    Add logging helper function.

--- inc/common/class-helpers.php
+++ inc/common/class-helpers.php
@@ -23,5 +23,23 @@ class Helpers {
         echo "</pre>";
     }
 
+    function log($message, $file_path='')
+    {
+        $file_path = ( ($file_path == '') || !file_exists($file_path) ) ? WP_CONTENT_DIR . '/mbo_api.log' : $file_path;
+        $header = date('l dS \o\f F Y h:i:s A', strtotime("now")) . "\t ";
+
+        if (is_array($message)) {
+            $message = print_r($message, true);
+        }
+        $message .= "\n";
+        file_put_contents(
+            $file_path,
+            $header . $message,
+            FILE_APPEND | LOCK_EX
+        );
+    }
+}
+
+
 }
 ?>
