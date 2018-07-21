7c72eb8d246cb6043ef306bf5429d7e5f6242639
Mike iLL Kilmer <mike@mzoo.org>
2018-07-20 21:49:14 -0500

    Our staff list template added to repo.

---
+++ inc/frontend/views/staff_list_gallery.php
@@ -0,0 +1,22 @@
+<?php
+use MZ_Mindbody as NS;
+
+//foreach($data->staff as $staff):
+//    var_dump($staff->ImageURL);
+//endforeach;
+?>
+<?php foreach($data->staff as $staff): ?>
+<div class="col-lg-3 col-md-4 col-xs-6 mz-staff-thumb">
+    <a class="thumbnail" data-target="#mzStaffModal" data-staffImage="<?php echo rawUrlEncode($staff->ImageURL) ?>"
+    data-staffName="<?php echo $staff->Name; ?>"
+    data-siteID="<?php echo $staff->siteID; ?>"
+    data-staffID="<?php echo $staff->ID; ?>"
+    data-staffBio="<?php echo $staff->Bio; ?>" href="<?php echo NS\PLUGIN_NAME_URL . 'inc/frontend/views/modals/modal_descriptions.php'; ?>">
+        <img class="img-responsive mz-staff-image" src="<?php echo $staff->ImageURL; ?>" alt="<?php echo $staff->Name; ?>">
+        <div class="mz-staff-name"><?php echo $staff->Name; ?></div>
+    </a>
+</div>
+<?php endforeach; ?>
+
+<div class="modal fade" id="mzStaffModal" tabindex="-1" role="dialog" aria-labelledby="mzSmallModalLabel"
+     aria-hidden="true"></div>
