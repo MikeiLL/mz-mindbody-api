ee1a4de329a3e3b2fea7ebd27b590fc2ca8c78d1
Rob Taylor <rt@tdmedia.com>
2018-09-27 09:14:37 -0700

    fixes tag error

--- inc/frontend/views/schedule_container.php
+++ inc/frontend/views/schedule_container.php
@@ -6,6 +6,8 @@ use MZ_Mindbody\Inc\Core as Core;
 /**
  * This file is the wrapper for Horizontal and/or Grid classes.
  */
+
+?>
 <?php if ($data->atts['type'] == 'week') : ?>
 <div id="mzScheduleNavHolder">
     <a href="#" class="previous" data-offset="-1"><?php _e('Previous Week', 'mz-mindbody-api'); ?></a> -
