6556628e5bbe7d2b532dc9f550f843ca7588181b
Mike iLL Kilmer <mike@mzoo.org>
2020-04-29 14:20:27 -0500

    Start to build out.

--- inc/common/class-token-management.php
+++ inc/common/class-token-management.php
@@ -4,5 +4,15 @@ namespace MZ_Mindbody\Inc\Common;
 use MZ_Mindbody as NS;
 
 class Token_Management {
-	
+	/**
+	 * Intermittently fetch, store and serve mbo api UserTokens.
+	 * @since 2.5.7
+	 *
+	 * The MBO V6 API serves and requires UserTokens which expire after seven days. So
+	 * we will store a valid UserToken in the db options table, intermittently updating it.
+	 *
+	 * wp_schedule_event
+	 *
+	 *
+	 */
 }
