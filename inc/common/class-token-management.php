2a68706fd818e4640316cfb69d23dd19a9472469
Mike iLL Kilmer <mike@mzoo.org>
2020-04-29 21:27:46 -0500

    Store MBO AccessToken.

--- inc/common/class-token-management.php
+++ inc/common/class-token-management.php
@@ -7,11 +7,11 @@ use MZ_Mindbody\Inc\Common\Interfaces as Interfaces;
 
 class Token_Management extends Interfaces\Retrieve {
 	/**
-	 * Intermittently fetch, store and serve mbo api UserTokens.
+	 * Intermittently fetch, store and serve mbo api AccessTokens.
 	 * @since 2.5.7
 	 *
-	 * The MBO V6 API serves and requires UserTokens which expire after seven days. So
-	 * we will store a valid UserToken in the db options table, intermittently updating it.
+	 * The MBO V6 API serves and requires AccessTokens which expire after seven days. So
+	 * we will store a valid AccessToken in the db options table, intermittently updating it.
 	 *
 	 * wp_schedule_event
 	 *
@@ -29,17 +29,89 @@ class Token_Management extends Interfaces\Retrieve {
         else:
             $this->mbo_account = '-99';
         endif;
+        
     }
 	 
+	 /**
+     * 
+     * Serve MBO AccessToken either from WP option or from MBO API
+     *
+     * @since 2.5.7
+     *
+     *
+     * @return AccessToken string as administered by MBO Api
+     */
 	 public function get_mbo_results() {
-	 	
+
 	 	$mb = $this->instantiate_mbo_API();
 	 	
         if ( !$mb ) return false;
         
         $response = $mb->TokenIssue();
-                
+                        
         return $response->AccessToken;
         
+	 }
+	 
+	 
+	 /**
+     * 
+     * Serve MBO AccessToken either from WP option or from MBO API
+     *
+     * @since 2.5.7
+     *
+     * Check stored token for existence and time saved. If LT 12 hours old 
+     * serve, if not, get from MBO API and save via save_token.
+     *
+     * @return AccessToken string as administered by MBO Api
+     */
+	 public function serve_token() {
+	 
+	 	$stored_token = get_option('mz_mbo_token', ['stored_time' => '', 'AccessToken' => '']);
+	 	
+	 	if (!empty( $stored_token )) {
+	 	
+	 		$current = new \DateTime();
+	 		$current->format('Y-m-d H:i:s');
+	 		$twleve_hours_ago = clone $current;
+			$twleve_hours_ago->sub(new \DateInterval('P12H'));
+	 		
+	 		if ( $stored_token['stored_time'] < $twleve_hours_ago || empty($stored_token['AccessToken']) ) {
+	 		
+	 			return $stored_token['AccessToken'];
+	 			
+	 		} else {
+	 		
+	 			return $this->save_token();
+	 			
+	 		}
+	 		
+	 	}
+	 }
+	 
+	 /**
+     * 
+     * Save MBO AccessToken to WP option
+     *
+     * @since 2.5.7
+     *
+     * Update (or create) mz_mbo_token option which holds datetime object 
+     * as well as AccessToken string
+     *
+     * @return AccessToken string as administered by MBO Api
+     */
+	 private function save_token() {
+	 
+	 	$token = $this->get_mbo_results();
+	 	
+		$current = new \DateTime();
+		$current->format('Y-m-d H:i:s');
+		
+	 	update_option('mz_mbo_token', [
+	 		'stored_time' => $current, 
+	 		'AccessToken' => $token
+	 	]);
+	 	
+	 	return $token;
 	 }
 }
--- tests/test-token-management.php
+++ tests/test-token-management.php
@@ -31,5 +31,31 @@ class Tests_Token_Management extends WP_UnitTestCase {
         $this->assertTrue( ctype_alnum($result) );
         
 	}
+	
+	public function test_stored_token() {
+
+        parent::setUp();
+
+        $this->assertTrue(class_exists('MZ_Mindbody\Inc\Common\Token_Management'));
+        
+        $basic_options_set = array(
+            'mz_source_name' => MBOTests\Test_Options::$_MYSOURCENAME,
+            'mz_mindbody_password' => MBOTests\Test_Options::$_MYPASSWORD,
+            'mz_mbo_app_name' => MBOTests\Test_Options::$_MYAPPNAME,
+            'mz_mbo_api_key' => MBOTests\Test_Options::$_MYAPIKEY,
+            'mz_mindbody_siteID' => '-99'
+        );
+        
+        add_option( 'mz_mbo_basic', $basic_options_set, '', 'yes' );
+		
+        $basic_options = get_option('mz_mbo_basic');
+                
+        $tm = new MZ_Mindbody\Inc\Common\Token_Management;
+        
+        $result = $tm->get_mbo_results();
+        
+        $this->assertTrue( ctype_alnum($result) );
+        
+	}
 
 }
