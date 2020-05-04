8df4ea8d769d5d00e214d93dd3f551c133129178
Mike iLL Kilmer <mike@mzoo.org>
2020-05-04 17:34:02 -0500

    Add name to method array keys.

--- inc/libraries/class-mbo-v6-api.php
+++ inc/libraries/class-mbo-v6-api.php
@@ -18,7 +18,7 @@ class MBO_V6_API {
 	private $tokenRequestTries = 6;
 	
 	protected $headersBasic = [
-		'Content-Type' => 'application/json',
+		'Content-Type' => 'application/json; charset=utf-8',
 		'Api-Key' => '',
 		'SiteId' => '-99'
 	];
@@ -64,36 +64,43 @@ class MBO_V6_API {
 			'AppointmentService' => [
 				'AddApppointment' => [
 										'method' => 'POST',
+										'name' => 'AddApppointment', 
 										'endpoint' => $this->endpointAppointment . '/addappointment',
 										'headers' => $this->headersBasic
 									 ],
 				'GetActiveSessionTimes' => [
 										'method' => 'GET',
+										'name' => 'GetActiveSessionTimes', 
 										'endpoint' => $this->endpointAppointment . '/activesessiontimes',
 										'headers' => $this->headersBasic
 									 ],
 				'GetAppointmentOptions' => [
 										'method' => 'GET',
+										'name' => 'GetAppointmentOptions', 
 										'endpoint' => $this->endpointAppointment . '/appointmentoptions',
 										'headers' => $this->headersBasic
 									 ],
 				'GetBookableItems' => [
 										'method' => 'GET',
+										'name' => 'GetBookableItems', 
 										'endpoint' => $this->endpointAppointment . '/bookableitems',
 										'headers' => $this->headersBasic
 									 ],
 				'GetScheduleItems' => [
 										'method' => 'GET',
+										'name' => 'GetScheduleItems', 
 										'endpoint' => $this->endpointAppointment . '/scheduleitems',
 										'headers' => $this->headersBasic
 									 ],
 				'GetStaffAppointments' => [
 										'method' => 'GET',
+										'name' => 'GetStaffAppointments', 
 										'endpoint' => $this->endpointAppointment . '/staffappointments',
 										'headers' => $this->headersBasic
 									 ],
 				'UpdateApppointment' => [
 										'method' => 'POST',
+										'name' => 'UpdateApppointment', 
 										'endpoint' => $this->endpointAppointment . '/updateappointment',
 										'headers' => $this->headersBasic
 									 ]
@@ -101,41 +108,49 @@ class MBO_V6_API {
 			'ClassService' => [
 				'AddClientToClass' => [
 										'method' => 'POST',
+										'name' => 'AddClientToClass', 
 										'endpoint' => $this->endpointClasses . '/addclienttoclass',
 										'headers' => $this->headersBasic
 									 ],
 				'GetClassDescriptions' => [
 										'method' => 'GET',
+										'name' => 'GetClassDescriptions', 
 										'endpoint' => $this->endpointClasses . '/classdescriptions',
 										'headers' => $this->headersBasic
 									 ],
 				'classGetClassVisits' => [
 										'method' => 'GET',
+										'name' => 'classGetClassVisits', 
 										'endpoint' => $this->endpointClasses . '/classvisits',
 										'headers' => $this->headersBasic
 									 ],
 				'GetClasses' => [
 										'method' => 'GET',
+										'name' => 'GetClasses', 
 										'endpoint' => $this->endpointClasses . '/classes',
 										'headers' => $this->headersBasic
 									 ],
 				'GetWaitlistEntries' => [
 										'method' => 'GET',
+										'name' => 'GetWaitlistEntries', 
 										'endpoint' => $this->endpointClasses . '/waitlistentries',
 										'headers' => $this->headersBasic
 									 ],
 				'RemoveClientFromClass' => [
 										'method' => 'POST',
+										'name' => 'RemoveClientFromClass', 
 										'endpoint' => $this->endpointClasses . '/removeclientfromclass',
 										'headers' => $this->headersBasic
 									 ],
 				'RemoveFromWaitlist' => [
 										'method' => 'POST',
+										'name' => 'RemoveFromWaitlist', 
 										'endpoint' => $this->endpointClasses . '/removefromwaitlist',
 										'headers' => $this->headersBasic
 									 ],
 				'SubstituteClassTeacher' => [
 										'method' => 'POST',
+										'name' => 'SubstituteClassTeacher', 
 										'endpoint' => $this->endpointClasses . '/substituteclassteacher',
 										'headers' => $this->headersBasic
 									 ]
@@ -143,121 +158,145 @@ class MBO_V6_API {
 			'ClientApi' => [
 				'AddArrival' => [
 										'method' => 'POST',
+										'name' => 'AddArrival', 
 										'endpoint' => $this->endpointClient . '/addarrival',
 										'headers' => $this->headersBasic
 									 ],
 				'AddClient' => [
 										'method' => 'POST',
+										'name' => 'AddClient', 
 										'endpoint' => $this->endpointClient . '/addclient',
 										'headers' => $this->headersBasic
 									 ],
 				'AddContactLog' => [
 										'method' => 'POST',
+										'name' => 'AddContactLog', 
 										'endpoint' => $this->endpointClient . '/addcontactlog',
 										'headers' => $this->headersBasic
 									 ],
 				'GetActiveClientMemberships' => [
 										'method' => 'GET',
+										'name' => 'GetActiveClientMemberships', 
 										'endpoint' => $this->endpointClient . '/activeclientmemberships',
 										'headers' => $this->headersBasic
 									 ],
 				'GetClientAccountBalances' => [
 										'method' => 'GET',
+										'name' => 'GetClientAccountBalances', 
 										'endpoint' => $this->endpointClient . '/clientaccountbalances',
 										'headers' => $this->headersBasic
 									 ],
 				'GetClientContracts' => [
 										'method' => 'GET',
+										'name' => 'GetClientContracts', 
 										'endpoint' => $this->endpointClient . '/clientcontracts',
 										'headers' => $this->headersBasic
 									 ],
 				'GetClientFormulaNotes' => [
 										'method' => 'GET',
+										'name' => 'GetClientFormulaNotes', 
 										'endpoint' => $this->endpointClient . '/clientformulanotes',
 										'headers' => $this->headersBasic
 									 ],
 				'GetClientIndexes' => [
 										'method' => 'GET',
+										'name' => 'GetClientIndexes', 
 										'endpoint' => $this->endpointClient . '/clientindexes',
 										'headers' => $this->headersBasic
 									 ],
 				'GetClientPurchases' => [
 										'method' => 'GET',
+										'name' => 'GetClientPurchases', 
 										'endpoint' => $this->endpointClient . '/clientpurchases',
 										'headers' => $this->headersBasic
 									 ],
 				'GetClientReferralTypes' => [
 										'method' => 'GET',
+										'name' => 'GetClientReferralTypes', 
 										'endpoint' => $this->endpointClient . '/clientreferraltypes',
 										'headers' => $this->headersBasic
 									 ],
 				'GetClientServices' => [
 										'method' => 'GET',
+										'name' => 'GetClientServices', 
 										'endpoint' => $this->endpointClient . '/clientservices',
 										'headers' => $this->headersBasic
 									 ],
 				'GetClientVisits' => [
 										'method' => 'GET',
+										'name' => 'GetClientVisits', 
 										'endpoint' => $this->endpointClient . '/clientvisits',
 										'headers' => $this->headersBasic
 									 ],
 				'GetClients' => [
 										'method' => 'GET',
+										'name' => 'GetClients', 
 										'endpoint' => $this->endpointClient . '/clients',
 										'headers' => $this->headersBasic
 									 ],
 				'GetContactLogs' => [
 										'method' => 'GET',
+										'name' => 'GetContactLogs', 
 										'endpoint' => $this->endpointClient . '/contactlogs',
 										'headers' => $this->headersBasic
 									 ],
 				'GetCrossRegionalClientAssociations' => [
 										'method' => 'GET',
+										'name' => 'GetCrossRegionalClientAssociations', 
 										'endpoint' => $this->endpointClient . '/crossregionalclientassociations',
 										'headers' => $this->headersBasic
 									 ],
 				'GetCustomClientFields' => [
 										'method' => 'GET',
+										'name' => 'GetCustomClientFields', 
 										'endpoint' => $this->endpointClient . '/customclientfields',
 										'headers' => $this->headersBasic
 									 ],
 				'GetRequiredClientFields' => [
 										'method' => 'GET',
+										'name' => 'GetRequiredClientFields', 
 										'endpoint' => $this->endpointClient . '/requiredclientfields',
 										'headers' => $this->headersBasic
 									 ],
 				'SendPasswordResetEmail' => [
 										'method' => 'POST',
+										'name' => 'SendPasswordResetEmail', 
 										'endpoint' => $this->endpointClient . '/sendpasswordresetemail',
 										'headers' => $this->headersBasic
 									 ],
 				'UpdateClient' => [
 										'method' => 'POST',
+										'name' => 'UpdateClient', 
 										'endpoint' => $this->endpointClient . '/updateclient',
 										'headers' => $this->headersBasic
 									 ],
 				'UpdateClientService' => [
 										'method' => 'POST',
+										'name' => 'UpdateClientService', 
 										'endpoint' => $this->endpointClient . '/updateclientservice',
 										'headers' => $this->headersBasic
 									 ],
 				'UpdateClientVisit' => [
 										'method' => 'POST',
+										'name' => 'UpdateClientVisit', 
 										'endpoint' => $this->endpointClient . '/updateclientvisit',
 										'headers' => $this->headersBasic
 									 ],
 				'UpdateContactLog' => [
 										'method' => 'POST',
+										'name' => 'UpdateContactLog', 
 										'endpoint' => $this->endpointClient . '/updatecontactlog',
 										'headers' => $this->headersBasic
 									 ],
 				'UploadClientDocument' => [
 										'method' => 'POST',
+										'name' => 'UploadClientDocument', 
 										'endpoint' => $this->endpointClient . '/uploadclientdocument',
 										'headers' => $this->headersBasic
 									 ],
 				'UploadClientPhoto' => [
 										'method' => 'POST',
+										'name' => 'UploadClientPhoto', 
 										'endpoint' => $this->endpointClient . '/uploadclientphoto',
 										'headers' => $this->headersBasic
 									 ]
@@ -265,11 +304,13 @@ class MBO_V6_API {
 			'EnrollmentApi' => [
 				'AddClientToEnrollment' => [
 										'method' => 'POST',
+										'name' => 'AddClientToEnrollment', 
 										'endpoint' => $this->endpointEnrollment . '/addclienttoenrollment',
 										'headers' => $this->headersBasic
 									 ],
 				'GetEnrollments' => [
 										'method' => 'GET',
+										'name' => 'GetEnrollments', 
 										'endpoint' => $this->endpointEnrollment . '/enrollments',
 										'headers' => $this->headersBasic
 									 ]
@@ -277,11 +318,13 @@ class MBO_V6_API {
 			'PayrollApi' => [
 				'GetClassPayroll' => [
 										'method' => 'GET',
+										'name' => 'GetClassPayroll', 
 										'endpoint' => $this->endpointPayroll . '/classes',
 										'headers' => $this->headersBasic
 									 ],
 				'GetTimeClock' => [
 										'method' => 'GET',
+										'name' => 'GetTimeClock', 
 										'endpoint' => $this->endpointPayroll . '/timeclock',
 										'headers' => $this->headersBasic
 									 ]
@@ -289,51 +332,61 @@ class MBO_V6_API {
 			'SaleApi' => [
 				'CheckoutShoppingCart' => [
 										'method' => 'POST',
+										'name' => 'CheckoutShoppingCart', 
 										'endpoint' => $this->endpointSale . '/checkoutshoppingcart',
 										'headers' => $this->headersBasic
 									 ],
 				'GetAcceptedCardTypes' => [
 										'method' => 'GET',
+										'name' => 'GetAcceptedCardTypes', 
 										'endpoint' => $this->endpointSale . '/acceptedcardtypes',
 										'headers' => $this->headersBasic
 									 ],
 				'GetContracts' => [
 										'method' => 'GET',
+										'name' => 'GetContracts', 
 										'endpoint' => $this->endpointSale . '/contracts',
 										'headers' => $this->headersBasic
 									 ],
 				'GetCustomPaymentMethods' => [
 										'method' => 'GET',
+										'name' => 'GetCustomPaymentMethods', 
 										'endpoint' => $this->endpointSale . '/custompaymentmethods',
 										'headers' => $this->headersBasic
 									 ],
 				'GetGiftCards' => [
 										'method' => 'GET',
+										'name' => 'GetGiftCards', 
 										'endpoint' => $this->endpointSale . '/giftcards',
 										'headers' => $this->headersBasic
 									 ],
 				'GetProducts' => [
 										'method' => 'GET',
+										'name' => 'GetProducts', 
 										'endpoint' => $this->endpointSale . '/products',
 										'headers' => $this->headersBasic
 									 ],
 				'GetSales' => [
 										'method' => 'GET',
+										'name' => 'GetSales', 
 										'endpoint' => $this->endpointSale . '/sales',
 										'headers' => $this->headersBasic
 									 ],
 				'GetServices' => [
 										'method' => 'GET',
+										'name' => 'GetServices', 
 										'endpoint' => $this->endpointSale . '/services',
 										'headers' => $this->headersBasic
 									 ],
 				'PurchaseContract' => [
 										'method' => 'POST',
+										'name' => 'PurchaseContract', 
 										'endpoint' => $this->endpointSale . '/purchasecontract',
 										'headers' => $this->headersBasic
 									 ],
 				'PurchaseGiftCard' => [
 										'method' => 'POST',
+										'name' => 'PurchaseGiftCard', 
 										'endpoint' => $this->endpointSale . '/purchasegiftcard',
 										'headers' => $this->headersBasic
 									 ]
@@ -341,31 +394,37 @@ class MBO_V6_API {
 			'SiteApi' => [
 				'GetActivationCode' => [
 										'method' => 'GET',
+										'name' => 'GetActivationCode', 
 										'endpoint' => $this->endpointPayroll . '/activationcode',
 										'headers' => $this->headersBasic
 									 ],
 				'GetLocations' => [
 										'method' => 'GET',
+										'name' => 'GetLocations', 
 										'endpoint' => $this->endpointPayroll . '/locations',
 										'headers' => $this->headersBasic
 									 ],
 				'GetPrograms' => [
 										'method' => 'GET',
+										'name' => 'GetPrograms', 
 										'endpoint' => $this->endpointPayroll . '/programs',
 										'headers' => $this->headersBasic
 									 ],
 				'GetResources' => [
 										'method' => 'GET',
+										'name' => 'GetResources', 
 										'endpoint' => $this->endpointPayroll . '/resources',
 										'headers' => $this->headersBasic
 									 ],
 				'GetSessionTypes' => [
 										'method' => 'GET',
+										'name' => 'GetSessionTypes', 
 										'endpoint' => $this->endpointPayroll . '/sessiontypes',
 										'headers' => $this->headersBasic
 									 ],
 				'GetSites' => [
 										'method' => 'GET',
+										'name' => 'GetSites', 
 										'endpoint' => $this->endpointPayroll . '/sites',
 										'headers' => $this->headersBasic
 									 ]
@@ -373,11 +432,13 @@ class MBO_V6_API {
 			'StaffApi' => [
 				'GetStaff' => [
 										'method' => 'GET',
+										'name' => 'GetStaff', 
 										'endpoint' => $this->endpointStaff . '/staff',
 										'headers' => $this->headersBasic
 									 ],
 				'GetStaffPermissions' => [
 										'method' => 'GET',
+										'name' => 'GetStaffPermissions', 
 										'endpoint' => $this->endpointStaff . '/staffpermissions',
 										'headers' => $this->headersBasic
 									 ]
@@ -385,11 +446,13 @@ class MBO_V6_API {
 			'UserToken' => [
 				'TokenIssue' => [
 										'method' => 'POST',
+										'name' => 'TokenIssue', 
 										'endpoint' => $this->endpointUserToken . '/issue',
 										'headers' => $this->headersBasic
 									 ],
 				'TokenRevoke' => [
 										'method' => 'DELETE',
+										'name' => 'TokenRevoke', 
 										'endpoint' => $this->endpointUserToken . '/revoke',
 										'headers' => $this->headersBasic
 									 ]
@@ -474,9 +537,16 @@ class MBO_V6_API {
 					
 		$request_body['Access'] = $this->tokenRequest( $restMethod );
 		
-		print_r('%%%% got a tokenRequest: %%%%');
-		print_r($request_body);
+		//if ($restMethod == 'AddClient'){
+			print_r('REST METHOD ');
+			print_r($restMethod);
+			print_r('Gets: ');
+			// print_r($request_body);
+			print_r($request_body);
+		//}
 		
+		// For some reason, for this call, we don't want to convert
+		// 'body' into a json string, as we do in the token request
 		$response = wp_remote_post( $restMethod['endpoint'], 
 			array(
 				'method' => $restMethod['method'],
@@ -484,20 +554,39 @@ class MBO_V6_API {
 				'httpversion' => '1.0',
 				'blocking' => true,
 				'headers' => $restMethod['headers'],
-				'body' => json_encode($request_body),
+				'body' => $request_body,
+				'data_format' => 'body',
 				'cookies' => array()
 			) );
+		
+		$response_body = json_decode($response['body'], true);
+		
+		// print_r('%%%% response body array keys: %%%%');
+		// print_r(array_keys(json_decode($response['body'], true)));
+		// print_r('%%%% response body array keys: %%%%');
+		// print_r(json_decode($response['body'], true)['PaginationResponse']);
+		// print_r('%%%% response body array keys: %%%%');
+		// print_r(array_keys(json_decode($response['body'], true)['Classes'][0]));
+		// print_r('%%%% response : %%%%');
+		// print_r($response['response']);
 
 		if ( is_wp_error( $response ) ) {
 			$error_message = $response->get_error_message();
 			return "Something went wrong: " . $error_message;
 		} else {
-			if ($response['response']['code'] != 200) {
-				// There may be a reason to do this at some point
-				return json_decode($response['body']);
-			} else {
-				return json_decode($response['body']);	
+
+		if ( array_key_exists( 'Error', $response_body ) && strpos($response_body['Error']["Message"], 'Please try again') ) {
+				// OK try again after three seconds
+				sleep(3);
+				if($this->tokenRequestTries > 1) {
+					return $this->callMindbodyService($restMethod);
+				}
+				return false;
 			}
+			
+		// return the data as an array, which is what we are used to
+		return $response_body;
+		
 		}
 	}
 	
@@ -545,7 +634,8 @@ class MBO_V6_API {
 			
 		} else {
 				if ( property_exists( $response_body, 'Error' ) && strpos($response_body->Error->Message, 'Please try again') ) {
-					// OK try again
+					// OK try again after three seconds
+					sleep(3);
 					if($this->tokenRequestTries > 1) {
 						return $this->tokenRequest($restMethod);
 					}
