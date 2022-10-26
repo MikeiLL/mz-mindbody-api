5bcae8073c98e19f647c015c4671ef2ac6d335ae
mikeill <mike@mzoo.org>
2022-10-26 12:42:47 -0500

    Begin to migrate Session and Client interfaces back to parent.

---
+++ src/Client/RetrieveClient.php
@@ -0,0 +1,548 @@
+<?php
+namespace MZoo\MzMindbody\Client;
+
+use MZoo\MzMindbody\Core as Core;
+use MZoo\MzMindbody\Common as Common;
+use MZoo\MzMindbody\Libraries as Libraries;
+use MZoo\MzMindbody\Schedule as Schedule;
+use MZoo\MzMindbody\Common\Interfaces as Interfaces;
+use MZoo\MzMindbody as NS;
+
+/*
+ * Class that holds Client Interface Methods
+ *
+ *
+ */
+class RetrieveClient extends Interfaces\Retrieve {
+
+    /**
+     * The Mindbody API Object
+     *
+     * @access private
+     */
+    private $mb;
+
+    /**
+     * Client ID
+     *
+     * The MBO ID of the Current User/Client
+     *
+     * @access private
+     */
+    private $clientID;
+
+    /**
+     * MBO Client
+     *
+     * GetClient result from MBO
+     *
+     * @access private
+     */
+    private $mbo_client;
+
+    /**
+     * Client Services
+     *
+     * Services returned from MBO
+     *
+     * @access private
+     */
+    private $services;
+
+    /**
+     * Format for date display, specific to MBO API Plugin.
+     *
+     * @since    2.4.7
+     * @access   public
+     * @var      string $date_format WP date format option.
+     */
+    public $date_format;
+
+    /**
+     * Format for time display, specific to MBO API Plugin.
+     *
+     * @since    2.4.7
+     * @access   public
+     * @var      string $time_format
+     */
+    public $time_format;
+
+    /**
+     * Class constructor
+     *
+     * Since 2.4.7
+     */
+    public function __construct(){
+        $this->date_format = Core\MzMindbodyApi::$date_format;
+        $this->time_format = Core\MzMindbodyApi::$time_format;
+    }
+
+    /**
+     * Client Login – using API VERSION 5!
+     *
+     * Since 2.5.7
+     *
+     * @param array $credentials with username and password
+     *
+     * @return array - result type and message
+     */
+    public function log_client_in( $credentials = ['username' => '', 'password' => ''] ){
+
+        $validateLogin = $this->validate_client($credentials);
+
+		if ( !empty($validateLogin['ValidateLoginResult']['GUID']) ) {
+			if ( $this->create_client_session( $validateLogin ) ) {
+				return ['type' => 'success', 'message' => __('Welcome', 'mz-mindbody-api') . ', ' . $validateLogin['ValidateLoginResult']['Client']['FirstName'] . '.<br/>'];
+			}
+			return ['type' => 'error', 'message' => sprintf(__('Whoops. Please try again, %1$s.', 'mz-mindbody-api'),
+            					$validateLogin['ValidateLoginResult']['Client']['FirstName'])];
+		} else {
+			// Otherwise error message
+			if ( !empty($validateLogin['ValidateLoginResult']['Message'] ) ) {
+
+				return ['type' => 'error', 'message' => $validateLogin['ValidateLoginResult']['Message']];
+
+			} else {
+				// Default fallback message.
+				return ['type' => 'error', 'message' => __('Invalid Login', 'mz-mindbody-api') . '<br/>'];
+
+			}
+		}
+	}
+
+
+    /**
+     * Validate Client - API VERSION 5!
+     *
+     * Since 2.5.7
+     *
+     * @param $validateLoginResult array with result from MBO API
+     */
+    public function validate_client( $validateLoginResult ){
+
+		// Create the MBO Object using API VERSION 5!
+        $this->get_mbo_results(5);
+
+		$result = $this->mb->ValidateLogin(array(
+			'Username' => $validateLoginResult['Username'],
+			'Password' => $validateLoginResult['Password']
+		));
+
+		return $result;
+
+    }
+
+
+    /**
+     * Create Client Session
+     *
+     * Since 2.5.7
+     *
+     * @param $validateLoginResult array with MBO result
+     */
+    public function create_client_session( $validateLoginResult ){
+
+		if (!empty($validateLoginResult['ValidateLoginResult']['GUID'])) {
+
+			// If validated, create session variables and store
+			$client_details = array(
+				'mbo_result' => $validateLoginResult['ValidateLoginResult']['Client']
+			);
+			NS\MZMBO()->session->set( 'MBO_Client', $client_details );
+
+			return true;
+
+		}
+
+    }
+
+    /**
+     * Client Log Out
+     */
+    public function client_log_out(){
+
+        NS\MZMBO()->session->clear();
+
+        return true;
+    }
+
+    /**
+     * Return MBO Account config required fields with what I think are default required fields.
+     *
+     * since: 2.5.7
+     *
+     * return array numeric array of required fields
+     */
+    public function get_signup_form_fields(){
+
+        // Crate the MBO Object
+        $this->get_mbo_results();
+
+        $requiredFields = $this->mb->GetRequiredClientFields();
+
+        $default_required_fields = [
+        	"Email",
+        	"FirstName",
+        	"LastName"
+        ];
+
+        return array_merge($default_required_fields, $requiredFields['RequiredClientFields']);
+    }
+
+    /**
+     * Create MBO Account
+     */
+    public function add_client( $client_fields = array() ){
+
+        // Crate the MBO Object
+        $this->get_mbo_results();
+
+		$signup_result = $this->mb->AddClient($client_fields);
+
+		return $signup_result;
+
+    }
+
+    /**
+     * Get client details.
+     *
+     * since: 2.5.7
+     *
+     * return array of client info from MBO or require login
+     */
+    public function get_client_details() {
+
+    	$client_info = NS\MZMBO()->session->get('MBO_Client');
+
+    	if (empty($client_info)) return __('Please Login', 'mz-mindbody-api');
+
+    	return $client_info['mbo_result'];
+
+    }
+
+    /**
+     * Get client active memberships.
+     *
+     * Memberships will be an array, each of which contain among other stuff:
+     *
+     * [Name] => Monthly Membership - Gym Access
+     *      [PaymentDate] => 2020-05-06T00:00:00
+     *      [Program] => Array
+     *          (
+     *              [Id] => 21
+     *              [Name] => Gym Membership
+     *              [ScheduleType] => Arrival
+     *              [CancelOffset] => 0
+     *          )
+     * [Remaining] => 1000, etc..
+     *
+     * since: 2.5.7
+     *
+     * return array numeric array of active memberships
+     */
+    public function get_client_active_memberships() {
+
+    	$client = $this->get_client_details();
+
+        // Create the MBO Object
+        $this->get_mbo_results();
+
+		$result = $this->mb->GetActiveClientMemberships(['clientId' => $client['ID']]); // UniqueID ??
+
+		return $result['ClientMemberships'];
+    }
+
+    /**
+     * Get client account balance.
+     *
+     * since: 2.5.7
+     *
+     * This wraps a method for getting balances for multiple accounts, but
+     * we just get it for one.
+     *
+     * return string client account balance
+     */
+    public function get_client_account_balance() {
+
+    	$client = $this->get_client_details();
+
+        // Create the MBO Object
+        $this->get_mbo_results();
+
+		// Can accept a list of client id strings
+		$result = $this->mb->GetClientAccountBalances(['clientIds' => $client['ID']]); // UniqueID ??
+
+		// Just return the first (and only) result
+		return $result['Clients'][0]['AccountBalance'];
+    }
+
+    /**
+     * Get client contracts.
+     *
+     * Since 2.5.7
+     *
+     * Returns an array of items that look like this:
+     *
+     * [AgreementDate] => 2020-05-06T00:00:00
+     * [AutopayStatus] => Active
+     * [ContractName] => Monthly Membership - 12 Months
+     * [EndDate] => 2021-05-06T00:00:00
+     * [Id] => 15040
+     * [OriginationLocationId] => 1
+     * [StartDate] => 2020-05-06T00:00:00
+     * [SiteId] => -99
+     * [UpcomingAutopayEvents] => Array
+     *     (
+     *         [0] => Array
+     *             (
+     *                 [ClientContractId] => 15040
+     *                 [ChargeAmount] => 75
+     *                 [PaymentMethod] => DebitAccount
+     *                 [ScheduleDate] => 2020-06-06T00:00:00
+     *             )
+     * etc...
+     * [LocationId] => 1
+	 * [Payments] => Array
+	 * (
+	 * 	[0] => Array
+	 * 		(
+	 * 			[Id] => 158015
+	 * 			[Amount] => 75
+	 * 			[Method] => 16
+	 * 			[Type] => Account
+	 * 			[Notes] =>
+	 * 		)
+	 *
+	 * )
+     *
+     * return array numeric array of client contracts
+     */
+    public function get_client_contracts() {
+
+    	$client = $this->get_client_details();
+
+        // Create the MBO Object
+        $this->get_mbo_results();
+
+		$result = $this->mb->GetClientContracts(['clientId' => $client['ID']]); // UniqueID ??
+
+		return $result['Contracts'];
+    }
+
+    /**
+     * Get client purchases.
+     *
+     * Since 2.5.7
+     *
+     * Returns an array of items that look like this:
+     * [Sale] => Array
+     *     (
+     *         [Id] => 100160377
+     *         [SaleDate] => 2020-05-06T00:00:00Z
+     *         [SaleTime] => 23:46:45
+     *         [SaleDateTime] => 2020-05-06T23:46:45Z
+     *         [ClientId] => 100015683
+     *         [PurchasedItems] => Array
+     *             (
+     *                 [0] => Array
+     *                     (
+     *                         [Id] => 1198
+     *                         [IsService] => 1
+     *                         [BarcodeId] =>
+     *                     )
+     *             )
+     *         [LocationId] => 1
+     *         [Payments] => Array
+     *             (
+     *                 [0] => Array
+     *                     (
+     *                         [Id] => 158015
+     *                         [Amount] => 75
+     *                         [Method] => 16
+     *                         [Type] => Account
+     *                         [Notes] =>
+     *                     )
+     *             )
+     *     )
+     * [Description] => Monthly Membership - Gym Access
+     * [AccountPayment] =>
+     * [Price] => 75
+     * [AmountPaid] => 75
+     * [Discount] => 0
+     * [Tax] => 0
+     * [Returned] =>
+     * [Quantity] => 1
+     *
+     * return array numeric array of client purchases
+     */
+    public function get_client_purchases() {
+
+    	$client = $this->get_client_details();
+
+        // Create the MBO Object
+        $this->get_mbo_results();
+
+		$result = $this->mb->GetClientPurchases(['clientId' => $client['ID']]); // UniqueID ??
+
+		return $result['Purchases'];
+    }
+
+    /**
+     * Get client services.
+     *
+     * since: 2.5.7
+     *
+     * return array numeric array of required fields
+     */
+    public function get_client_services() {
+
+    	$client = $this->get_client_details();
+
+        // Create the MBO Object
+        $this->get_mbo_results();
+
+		$result = $this->mb->GetClientServices(['clientId' => $client['ID']]); // UniqueID ??
+
+		return $result;
+    }
+
+    /**
+     * Create MBO Account
+     * since 5.4.7
+     *
+     * param array containing 'UserEmail' 'UserFirstName' 'UserLastName'
+     *
+     * return array either error or new client details
+     */
+    public function password_reset_email_request( $clientID = array() ){
+
+        // Crate the MBO Object
+        $this->get_mbo_results();
+
+		$result = $this->mb->SendPasswordResetEmail($clientID);
+
+		return $result;
+
+    }
+
+
+    /**
+     * Check Client Logged In
+     *
+     * Since 2.5.7
+     * Is there a session containing the MBO_GUID of current user
+     *
+     * @return bool
+     */
+    public function check_client_logged(){
+
+        return ( 1 == (bool) NS\MZMBO()->session->get('MBO_Client') ) ? 1 : 0;
+
+    }
+
+    /**
+     * Get API version, create API Interface Object
+     *
+     * @since 2.4.7
+     *
+     * @param $api_version int in case we need to call on API v5 as in for client login
+     *
+     *
+     * @return array of MBO schedule data
+     */
+    public function get_mbo_results( $api_version = 6 ){
+
+		if ( $api_version == 6 ) {
+        	$this->mb = $this->instantiate_mbo_API();
+		} else {
+			$this->mb = $this->instantiate_mbo_API( 5 );
+		}
+
+        if ( !$this->mb || $this->mb == 'NO_API_SERVICE' ) return false;
+
+        return true;
+    }
+
+     /**
+     * Return an array of MBO Class Objects, ordered by date, then time.
+     *
+     * This is a limited version of the Retrieve Classes method used in horizontal schedule
+     *
+     *
+     * @param @type array $mz_classes
+     *
+     * @return @type array of Objects from Single_event class, in Date (and time) sequence.
+     */
+    public function sort_classes_by_date_then_time($client_schedule = array()) {
+
+        $classesByDateThenTime = array();
+
+        /* For some reason, when there is only a single class in the client
+         * schedule, the 'Visits' array contains that visit, but when there are multiple
+         * visits then the array of visits is under 'Visits'/'Visit'
+         */
+        if (is_array($client_schedule['GetClientScheduleResult']['Visits']['Visit'][0])){
+            // Multiple visits
+            $visit_array_scope = $client_schedule['GetClientScheduleResult']['Visits']['Visit'];
+        } else {
+            $visit_array_scope = $client_schedule['GetClientScheduleResult']['Visits'];
+        }
+
+
+        foreach($visit_array_scope as $visit)
+        {
+            // Make a timestamp of just the day to use as key for that day's classes
+            $dt = new \DateTime($visit['StartDateTime']);
+            $just_date =  $dt->format('Y-m-d');
+
+            /* Create a new array with a key for each date YYYY-MM-DD
+            and corresponding value an array of class details */
+
+            $single_event = new Schedule\Mini_Schedule_Item($visit);
+
+            if(!empty($classesByDateThenTime[$just_date])) {
+                array_push($classesByDateThenTime[$just_date], $single_event);
+            } else {
+                $classesByDateThenTime[$just_date] = array($single_event);
+            }
+        }
+
+        /* They are not ordered by date so order them by date */
+        ksort($classesByDateThenTime);
+
+        foreach($classesByDateThenTime as $classDate => &$classes)
+        {
+            /*
+             * $classes is an array of all classes for given date
+             * Take each of the class arrays and order it by time
+             * $classesByDateThenTime should have a length of seven, one for
+             * each day of the week.
+             */
+            usort($classes, function($a, $b) {
+                if($a->startDateTime == $b->startDateTime) {
+                    return 0;
+                }
+                return $a->startDateTime < $b->startDateTime ? -1 : 1;
+            });
+        }
+
+        return $classesByDateThenTime;
+    }
+
+
+    /**
+     * Make Numeric Array
+     *
+     * Make sure that we have an array
+     *
+     * @param $data
+     * @return array
+     */
+    private function make_numeric_array($data) {
+
+        return (isset($data[0])) ? $data : array($data);
+
+    }
+
+
+}
---
+++ src/Session/MzAccessSession.php
@@ -0,0 +1,300 @@
+<?php
+/**
+ * MzAccessSession wrapper class.
+ *
+ * Wrap Eric Mann's sophisticated WP_Session class.
+ *
+ * @package MZMBOACCESS
+ */
+
+namespace MZoo\MzMindbody\Session;
+
+use MZoo\MzMboAccess\Dependencies\EAMann\Sessionz;
+use MZoo\MzMboAccess\Dependencies\EAMann\WPSession;
+
+/**
+ * MzAccessSession wrapper class
+ *
+ * @since 1.0.1
+ */
+class MzAccessSession {
+
+	/**
+	 * Hold the class instance.
+	 *
+	 * @var class instance
+	 * @access private
+	 * @since  1.0.1
+	 */
+	private static $instance = null;
+
+	/**
+	 * Holds our session data
+	 *
+	 * @var    array
+	 * @access public
+	 * @since  1.0.1
+	 */
+	public $session;
+
+	/**
+	 * Session index prefix
+	 *
+	 * @since  1.0.1
+	 * @access private
+	 *
+	 * @var string
+	 */
+	private $prefix = 'mz_mbo_access_';
+
+	/**
+	 * Get things started
+	 *
+	 * Defines our WP_Session constants, includes the necessary libraries and
+	 * retrieves the WP Session instance
+	 *
+	 * @since 1.0.1
+	 */
+	public function __construct() {
+
+		if ( ! $this->should_start_session() ) {
+			return;
+		}
+        // There were also issues with running PHPUNIT and getting
+        // Headers already sent warnings, which broke the tests.
+        // May, at some point, want to instantiate the sessions
+        // Separately from main plugin instance using
+        // add_action( 'template_redirect'
+		if ( PHP_SESSION_DISABLED !== session_status() &&
+        ( ! defined( 'WP_CLI' ) || false === WP_CLI ) &&
+        ( ! defined( 'RUNNING_PHPUNIT' ) || false === RUNNING_PHPUNIT ) ) {
+			add_action( 'wp_loaded', array( $this, 'wp_session_manager_initialize' ), 1, 0 );
+			// If we're not in a cron, start the session.
+			if ( ! defined( 'DOING_CRON' ) || false === DOING_CRON ) {
+				add_action( 'wp_loaded', array( $this, 'wp_session_manager_start_session' ), 10, 0 );
+			}
+		}
+	}
+
+
+	/**
+	 * Return our session instance
+	 * @since 2.9.9
+	 */
+	public function get_session() {
+		return $this->session;
+	}
+
+
+	/**
+	 * Initialize the plugin, bootstrap autoloading, and register default hooks
+	 */
+	public function wp_session_manager_initialize() {
+
+		if ( ! isset( $_SESSION ) ) {
+			// Queue up the session stack.
+			$wp_session_handler = Sessionz\Manager::initialize();
+
+			// Fall back to database storage where needed.
+			if ( defined( 'WP_SESSION_USE_OPTIONS' ) && WP_SESSION_USE_OPTIONS ) {
+				$wp_session_handler->addHandler( new WPSession\OptionsHandler() );
+			} else {
+				$wp_session_handler->addHandler( new WPSession\DatabaseHandler() );
+
+				/**
+				 * The database handler can automatically clean up sessions as it goes. By default,
+				 * we'll run the cleanup routine every hour to catch any stale sessions that PHP's
+				 * garbage collector happens to miss. This timeout can be filtered to increase or
+				 * decrease the frequency of the manual purge.
+				 *
+				 * @param string $timeout Interval with which to purge stale sessions
+				 */
+				$timeout = apply_filters( 'wp_session_gc_interval', 'hourly' );
+
+				if ( ! wp_next_scheduled( 'wp_session_database_gc' ) ) {
+					wp_schedule_event( time(), $timeout, 'wp_session_database_gc' );
+				}
+
+				add_action( 'wp_session_database_gc', array( 'EAMann\WPSession\DatabaseHandler', 'directClean' ) );
+			}
+
+			// If we have an external object cache, let's use it!
+			if ( wp_using_ext_object_cache() ) {
+				$wp_session_handler->addHandler( new WPSession\CacheHandler() );
+			}
+
+			if ( defined( 'WP_SESSION_ENC_KEY' ) && WP_SESSION_ENC_KEY ) {
+				$wp_session_handler->addHandler( new Sessionz\Handlers\EncryptionHandler( WP_SESSION_ENC_KEY ) );
+			}
+
+			// Use an in-memory cache for the instance if we can. This will only help in rare cases.
+			$wp_session_handler->addHandler( new Sessionz\Handlers\MemoryHandler() );
+
+			$_SESSION['wp_session_manager'] = 'active';
+		}
+
+		if ( 'active' !== $_SESSION['wp_session_manager'] || ! isset( $_SESSION['wp_session_manager'] ) ) {
+			add_action( 'admin_notices', array( $this, 'wp_session_manager_multiple_sessions_notice' ) );
+			return;
+		}
+
+		// Create the required table.
+		WPSession\DatabaseHandler::createTable();
+
+		register_deactivation_hook(
+			__FILE__,
+			function () {
+				wp_clear_scheduled_hook( 'wp_session_database_gc' );
+			}
+		);
+	}
+
+	/**
+	 * Determines if we should start sessions
+	 *
+	 * @return bool
+	 * @since  1.0.1
+	 */
+	public function should_start_session() {
+
+		$start_session = true;
+		if ( ! empty( $_SERVER['REQUEST_URI'] ) ) {
+			$blacklist = $this->get_blacklist();
+			$uri       = ltrim( $_SERVER['REQUEST_URI'], '/' );
+			$uri       = untrailingslashit( $uri );
+			if ( in_array( $uri, $blacklist, true ) ) {
+				$start_session = false;
+			}
+			if ( false !== strpos( $uri, 'feed=' ) ) {
+				$start_session = false;
+			}
+		}
+
+		return apply_filters( 'mbo_access_sessions_start_session', $start_session );
+	}
+
+	/**
+	 * Retrieve the URI blacklist
+	 *
+	 * These are the URIs where we never start sessions
+	 *
+	 * @return array
+	 * @since  1.0.1
+	 */
+	public function get_blacklist() {
+		$blacklist = apply_filters(
+			'mbo_access_sessions_session_start_uri_blacklist',
+			array(
+				'feed',
+				'feed/rss',
+				'feed/rss2',
+				'feed/rdf',
+				'feed/atom',
+				'comments/feed',
+			)
+		);
+		// Look to see if WordPress is in a sub folder or this is a network site that uses sub folders.
+		$folder = str_replace( network_home_url(), '', get_site_url() );
+		if ( ! empty( $folder ) ) {
+			foreach ( $blacklist as $path ) {
+				$blacklist[] = $folder . '/' . $path;
+			}
+		}
+
+		return $blacklist;
+	}
+
+	/**
+	 * Main MzAccessSession Instance
+	 *
+	 * Ensures only one instance is loaded or can be loaded.
+	 *
+	 * @return MzAccessSession - Main instance
+	 * @since  1.0.1
+	 * @static
+	 */
+	public static function instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * If a session hasn't already been started by some external system, start one!
+	 */
+	public function wp_session_manager_start_session() {
+		if ( session_status() !== PHP_SESSION_ACTIVE ) {
+			session_start();
+		}
+	}
+
+	/**
+	 * Print an admin notice if too many plugins are manipulating sessions.
+	 *
+	 * @global array $this->wp_session_messages
+	 */
+	public function wp_session_manager_multiple_sessions_notice() {
+		echo '<div class="notice notice-error">';
+		echo '<p>';
+		esc_html_e(
+			'Another plugin is attempting to start a session with WordPress. WP Session Manager will not work!',
+			'wp-session-manager'
+		);
+		echo '</p>';
+		echo '</div>';
+	}
+
+
+	/**
+	 * Cloning is forbidden.
+	 *
+	 * @since 1.0.1
+	 */
+	public function __clone() {
+		_doing_it_wrong( __FUNCTION__, esc_html_e( 'Cheatin&#8217; huh?', 'wsi' ), '2.1' );
+	}
+
+	/**
+	 * Unserializing instances of this class is forbidden.
+	 *
+	 * @since 1.0.1
+	 */
+	public function __wakeup() {
+		_doing_it_wrong( __FUNCTION__, esc_html_e( 'Cheatin&#8217; huh?', 'wsi' ), '2.1' );
+	}
+
+	/**
+	 * Retrieve a session variable
+	 *
+	 * @access public
+	 *
+	 * @param string $key Session key.
+	 *
+	 * @return mixed Session variable
+	 * @since  1.5
+	 */
+	public function get( $key ) {
+		$key = sanitize_key( $key );
+
+		return isset( $_SESSION[ $key ] ) ? json_decode( $_SESSION[ $key ] ) : false;
+	}
+
+	/**
+	 * Set a session variable
+	 *
+	 * @param string           $key   Session key.
+	 * @param int|string|array $value Session variable.
+	 *
+	 * @return mixed Session variable
+	 * @since  1.0.1
+	 */
+	public function set( $key, $value ) {
+		$key = sanitize_key( $key );
+
+		$_SESSION[ $key ] = wp_json_encode( $value );
+
+		return $_SESSION[ $key ];
+	}
+}
