5fd7a4a8e3f03f82880fe203db5831383845bf85
mikeill <mike@mzoo.org>
2026-03-23 18:42:07 -0500

    Bulk file escaping.

--- src/Frontend/views/create_mbo_account.php
+++ src/Frontend/views/create_mbo_account.php
@@ -1,59 +1,91 @@
 <?php
 /**
- * Create MBO Account Form.
- *
- * Over-ride-able template page for full event listing shortcode result.
+ * Event Listing List
  *
  * @package MzMindbody
+ *
+ * This file contains the template for events listed in a tabular list.
  */
 
-?>
-<div style="max-width:90%;margin: 0 auto;">
+use MZoo\MzMindbody as NS;
+use MZoo\MzMindbody\Core as Core;
 
-    <h3><?php esc_html_e( 'Create MBO Account', 'mz-mindbody-api' ); ?></h3>
+/* TODO: subclass maybe to DRY up btwn this and event_listing_full. */
 
-    <form style="margin:1em 0;" class="mz_mbo_signup" data-async id="mzSignUp" data-target="#mzSignUpModal" method="POST">
+if ( ( is_array( $data->events ) ) && ! empty( $data->events ) ) : ?>
+    <table class="mz_event_list_listing">
 
-            <p class="website_url" style="display:none"><?php echo $antispam; ?><input type="text" name="website_url" /></p>
-            <input type="hidden" name="nonce" value="<?php echo $data->nonce; ?>"/>
-            <input type="hidden" name="classID" value="<?php echo $data->class_id; ?>" />
-            <label for="Username"> <?php echo $data->username; ?></label> <input type="text" name="data[Client][Username]" id="Username" placeholder="<?php echo $data->username; ?>" required /><br />
-            <label for="Email"> <?php echo $data->clientemail; ?></label> <input type="text" name="data[Client][Email]" id="Email" placeholder="<?php echo $data->clientemail; ?>" required /><br />
-            <label for="Password"> <?php echo $data->password; ?></label> <input type="password" name="data[Client][Password]" id="Password" placeholder="<?php echo $data->password; ?>" required /><br />
-            <label for="FirstName"> <?php echo $data->firstname; ?></label> <input type="text" name="data[Client][FirstName]" id="FirstName" placeholder="<?php echo $data->firstname; ?>" required /><br />
-            <label for="LastName"> <?php echo $data->lastname; ?></label> <input type="text" name="data[Client][LastName]" id="LastName" placeholder="<?php echo $data->lastname; ?>" required /><br />
-            <?php echo esc_html( $data->required_fields_inputs ); ?><br />
-            <button type="submit" class="btn btn-primary"><?php echo $data->sign_up; ?></button>
-                <?php echo esc_html( $data->clientemail ); ?>
-            </label>
-            <input type="text" name="data[Client][Email]" id="Email"
-                placeholder="<?php echo esc_html( $data->clientemail ); ?>" required />
-            <br />
-            <label for="Password">
-                <?php echo esc_html( $data->password ); ?>
-            </label>
-            <input type="password" name="data[Client][Password]" id="Password"
-                placeholder="<?php echo esc_html( $data->password ); ?>" required />
-            <br />
-            <label for="FirstName">
-                <?php echo esc_html( $data->firstname ); ?>
-            </label>
-            <input type="text" name="data[Client][FirstName]" id="FirstName"
-                placeholder="<?php echo esc_html( $data->firstname ); ?>" required />
-            <br />
-            <label for="LastName">
-                <?php echo esc_html( $data->lastname ); ?>
-            </label>
-            <input type="text" name="data[Client][LastName]" id="LastName"
-                placeholder="<?php echo esc_html( $data->lastname ); ?>" required />
-            <br />
-            <?php echo esc_html( $data->required_fields_inputs ); ?>
-            <br />
-            <button type="submit" class="btn btn-primary">
-                <?php echo esc_html( $data->sign_up ); ?>
-            </button>
+        <thead>
+            <tr class="header" style="display: table-row">
+                <th class="mz_event_date_display" scope="header">
+                    <?php echo esc_html($data->heading_date, 'mz-mindbody-api'); ?>
+                </th>
+                <th class="mz_event_classDetails" scope="header">
+                    <?php echo esc_html($data->heading_time, 'mz-mindbody-api'); ?>
+                </th>
+                <th class="mz_event_name" scope="header">
+                    <?php echo esc_html($data->heading_event, 'mz-mindbody-api'); ?>
+                </th>
+                <?php if ( $data->locations_count >= 2 ) : ?>
+                    <th class="mz_event_location" scope="header">
+                    <?php echo esc_html($data->heading_location, 'mz-mindbody-api'); ?>
+                    </th>
+                <?php endif; ?>
+            </tr>
+        </thead>
 
-    </form>
+    <?php foreach ( $data->events as $date => $events ) : ?>
+        <?php foreach ( $events as $event ) : ?>
+        <tbody>
+            <tr>
+                <td><?php echo esc_html(gmdate( Core\MzMindbodyApi::$date_format, strtotime( $event->start_datetime ) ), 'mz-mindbody-api'); ?>
+                <br /><?php echo wp_kses($event->sign_up_link->build(), [
+              'a' => [
+                'title' => [],
+                'href' => [],
+                'class' => [],
+                'id' => [],
+              ]
+            ]); ?></td>
+                <td>
+            <?php echo esc_html(gmdate( Core\MzMindbodyApi::$time_format, strtotime( $event->start_datetime ) ), 'mz-mindbody-api'); ?> -
+            <?php echo esc_html(gmdate( Core\MzMindbodyApi::$time_format, strtotime( $event->end_datetime ) ), 'mz-mindbody-api'); ?>
+                </td>
+                <td>
+            <?php echo wp_kses($event->class_name_link->build(), [
+              'a' => [
+                'title' => [],
+                'href' => [],
+                'class' => [],
+                'id' => [],
+              ]
+            ]) . ' ' . esc_html($data->with, 'mz-mindbody-api') . ' ' . wp_kses($event->staff_name_link->build(), [
+              'a' => [
+                'title' => [],
+                'href' => [],
+                'class' => [],
+                'id' => [],
+              ]
+            ]); ?>
 
+                </td>
+            <?php
+            // Display location if showing schedule for more than one location.
+            if ( $data->locations_count >= 2 ) :
+                ?>
+                <td>
+                <?php echo esc_url($data->locations_dictionary[ $event->location_id ]['link']); ?>
+                </td>
+            <?php endif; ?>
+            </tr>
+        <?php endforeach; ?>
+    <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php elseif ( 0 === count( $data->events ) ) : ?>
+    <h4><?php echo esc_html( $data->no_events, 'mz-mindbody-api' ); ?></h4>
+<?php else : ?>
+    <div class="error"><?php esc_html_e( 'Error Retrieving Events', 'mz-mindbody-api' ); ?></div>
+    <p><?php var_dump( $data->events ); ?></p>
 
-</div>
+<?php endif; ?>
--- src/Frontend/views/event_listing_list.php
+++ src/Frontend/views/event_listing_list.php
@@ -10,23 +10,25 @@
 use MZoo\MzMindbody as NS;
 use MZoo\MzMindbody\Core as Core;
 
+/* TODO: subclass maybe to DRY up btwn this and event_listing_full. */
+
 if ( ( is_array( $data->events ) ) && ! empty( $data->events ) ) : ?>
     <table class="mz_event_list_listing">
 
         <thead>
             <tr class="header" style="display: table-row">
                 <th class="mz_event_date_display" scope="header">
-                    <?php echo $data->heading_date; ?>
+                    <?php echo esc_html($data->heading_date, 'mz-mindbody-api'); ?>
                 </th>
                 <th class="mz_event_classDetails" scope="header">
-                    <?php echo $data->heading_time; ?>
+                    <?php echo esc_html($data->heading_time, 'mz-mindbody-api'); ?>
                 </th>
                 <th class="mz_event_name" scope="header">
-                    <?php echo $data->heading_event; ?>
+                    <?php echo esc_html($data->heading_event, 'mz-mindbody-api'); ?>
                 </th>
                 <?php if ( $data->locations_count >= 2 ) : ?>
                     <th class="mz_event_location" scope="header">
-                    <?php echo $data->heading_location; ?>
+                    <?php echo esc_html($data->heading_location, 'mz-mindbody-api'); ?>
                     </th>
                 <?php endif; ?>
             </tr>
@@ -36,13 +38,35 @@ if ( ( is_array( $data->events ) ) && ! empty( $data->events ) ) : ?>
         <?php foreach ( $events as $event ) : ?>
         <tbody>
             <tr>
-                <td><?php echo gmdate( Core\MzMindbodyApi::$date_format, strtotime( $event->start_datetime ) ); ?> <br /><?php echo $event->sign_up_link->build(); ?></td>
+                <td><?php echo esc_html(gmdate( Core\MzMindbodyApi::$date_format, strtotime( $event->start_datetime ) ), 'mz-mindbody-api'); ?>
+                <br /><?php echo wp_kses($event->sign_up_link->build(), [
+              'a' => [
+                'title' => [],
+                'href' => [],
+                'class' => [],
+                'id' => [],
+              ]
+            ]); ?></td>
                 <td>
-            <?php echo gmdate( Core\MzMindbodyApi::$time_format, strtotime( $event->start_datetime ) ); ?> -
-            <?php echo gmdate( Core\MzMindbodyApi::$time_format, strtotime( $event->end_datetime ) ); ?>
+            <?php echo esc_html(gmdate( Core\MzMindbodyApi::$time_format, strtotime( $event->start_datetime ) ), 'mz-mindbody-api'); ?> -
+            <?php echo esc_html(gmdate( Core\MzMindbodyApi::$time_format, strtotime( $event->end_datetime ) ), 'mz-mindbody-api'); ?>
                 </td>
                 <td>
-            <?php echo $event->class_name_link->build() . ' ' . $data->with . ' ' . $event->staff_name_link->build(); ?>
+            <?php echo wp_kses($event->class_name_link->build(), [
+              'a' => [
+                'title' => [],
+                'href' => [],
+                'class' => [],
+                'id' => [],
+              ]
+            ]) . ' ' . esc_html($data->with, 'mz-mindbody-api') . ' ' . wp_kses($event->staff_name_link->build(), [
+              'a' => [
+                'title' => [],
+                'href' => [],
+                'class' => [],
+                'id' => [],
+              ]
+            ]); ?>
 
                 </td>
             <?php
@@ -50,7 +74,7 @@ if ( ( is_array( $data->events ) ) && ! empty( $data->events ) ) : ?>
             if ( $data->locations_count >= 2 ) :
                 ?>
                 <td>
-                <?php echo $data->locations_dictionary[ $event->location_id ]['link']; ?>
+                <?php echo esc_url($data->locations_dictionary[ $event->location_id ]['link']); ?>
                 </td>
             <?php endif; ?>
             </tr>
@@ -59,7 +83,7 @@ if ( ( is_array( $data->events ) ) && ! empty( $data->events ) ) : ?>
         </tbody>
     </table>
 <?php elseif ( 0 === count( $data->events ) ) : ?>
-    <h4><?php echo esc_html( $data->no_events ); ?></h4>
+    <h4><?php echo esc_html( $data->no_events, 'mz-mindbody-api' ); ?></h4>
 <?php else : ?>
     <div class="error"><?php esc_html_e( 'Error Retrieving Events', 'mz-mindbody-api' ); ?></div>
     <p><?php var_dump( $data->events ); ?></p>
