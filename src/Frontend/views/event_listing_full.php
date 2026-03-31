2fa169d1f663b6d79350bc5d212d38c0aa914140
mikeill <mike@mzoo.org>
2026-03-30 20:29:45 -0500

    Escape event listing with wp_kses.

--- src/Frontend/views/event_listing_full.php
+++ src/Frontend/views/event_listing_full.php
@@ -42,26 +42,88 @@ if ( ( is_array( $data->events ) ) && ! empty( $data->events ) ) :
                 <?php echo esc_html( $event->class_name ); ?>
             </h3>
             <span class="mz_full_listing_event__staff">
-            <?php echo esc_html($data->with . ' ' . $event->staff_name_link->build()); ?>
+            <?php echo wp_kses($data->with . ' ' . $event->staff_name_link->build(), [
+              "a" => [
+                "href" => [],
+                "id" => [],
+                "class" => [],
+                "title" => [],
+              ]
+            ]); ?>
             </span>
             <?php
             // Display location if showing schedule for more than one location.
             if ( $data->locations_count >= 2 ) :
                 ?>
-                <span><?php echo esc_html( 'at', 'mz-mindbody-api' ) . ' ' . esc_url($data->locations_dictionary[ $event->location_id ]['link']); ?></span>
+                <span><?php echo esc_html( 'at', 'mz-mindbody-api' ) . ' ' . wp_kses($data->locations_dictionary[ $event->location_id ]['link'], [
+                  "a" => [
+                    "id" => [],
+                    "title" => [],
+                    "class" => [],
+                  ]
+                ]); ?></span>
             <?php endif; ?>
             <div class="mz_full_listing_event__date">
-            <?php echo esc_url( $event->start_date, 'mz-mindbody-api' ); ?>
-            <?php echo esc_url( $event->start_time, 'mz-mindbody-api' ); ?> –
-            <?php echo esc_url( $event->end_date, 'mz-mindbody-api' ); ?>
-            <?php echo esc_url( $event->end_time, 'mz-mindbody-api' ); ?>
+            <?php echo esc_html( $event->start_date, 'mz-mindbody-api' ); ?>
+            <?php echo esc_html( $event->start_time, 'mz-mindbody-api' ); ?> –
+            <?php echo esc_html( $event->end_date, 'mz-mindbody-api' ); ?>
+            <?php echo esc_html( $event->end_time, 'mz-mindbody-api' ); ?>
             </div>
             <div class="mz_full_listing_event__disc">
                 <p><img src="<?php echo esc_url($event->class_image); ?>" class="mz_inline_event_image_body" />
-            <?php echo esc_html( html_entity_decode($event->event_description), 'mz-mindbody-api' ) ; ?>
+            <?php echo wp_kses( html_entity_decode($event->event_description), [
+              "div" => [
+                "class" => [],
+                "id" => [],
+                "title" => [],
+              ],
+              "a" => [
+                "class" => [],
+                "id" => [],
+                "title" => [],
+                "href" => [],
+              ],
+              "span" => [
+                "class" => [],
+                "id" => [],
+                "title" => [],
+              ],
+              "p" => [
+                "class" => [],
+                "id" => [],
+                "title" => [],
+              ],
+              "h1" => [
+                "class" => [],
+                "id" => [],
+                "title" => [],
+              ],
+              "h2" => [
+                "class" => [],
+                "id" => [],
+                "title" => [],
+              ],
+              "h3" => [
+                "class" => [],
+                "id" => [],
+                "title" => [],
+              ],
+              "h4" => [
+                "class" => [],
+                "id" => [],
+                "title" => [],
+              ],
+              ] ) ; ?>
                 </p>
                 <p>
-            <?php echo esc_html($event->sign_up_link->build(), 'mz-mindbody-api' ); ?>
+            <?php echo wp_kses($event->sign_up_link->build(), [
+              "a" => [
+                "class" => [],
+                "id" => [],
+                "title" => [],
+                "href" => [],
+              ],
+            ] ); ?>
                 </p>
             </div>
             <hr class="mz_full_listing_event__rule" />
@@ -71,7 +133,9 @@ if ( ( is_array( $data->events ) ) && ! empty( $data->events ) ) :
     <?php endforeach; ?>
 
 <?php elseif ( 0 === (int) count( $data->events ) ) : ?>
-    <h4><?php echo esc_html($data->no_events, 'mz-mindbody-api'); ?></h4>
+    <h4><?php echo wp_kses($data->no_events, [
+      "span" => [],
+    ]); ?></h4>
 <?php else : ?>
     <div class="error"><?php echo esc_html( 'Error Retrieving Events', 'mz-mindbody-api' ); ?></div>
     <p><?php var_dump( $data->events ); ?></p>
